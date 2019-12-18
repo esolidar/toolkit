c5947803503211a2fb06a2dc0d675c4da317af38
unknown <patricia@esolidar.pt>
2019-12-18 14:59:31 +0000

    ESOLIDAR-248

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Components
-export * from './components/loading/Loading';
+export * from './components/changeCurrency/ChangeCurrency';
 export * from './components/changeLanguage/ChangeLanguage';
+export * from './components/footer/Footer';
 export * from './components/icon/Icon';
+export * from './components/loading/Loading';
 export * from './components/socialNetworks/SocialNetworks';
 
 // Elements
