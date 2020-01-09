f40f2a39766328a9669b230a00d8115b986e8a66
unknown <patricia@esolidar.pt>
2020-01-09 13:06:53 +0000

    comment add react-router-dom

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-// eslint-disable-next-line import/no-unresolved
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Button = ({
   extraClass, target, href, text, onClick, to,
