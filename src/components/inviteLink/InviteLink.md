69329c864f040e7305a46d523d54c970733c37ba
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2020-03-12 15:16:40 +0000

    Esolidar 387 (#14)
    * #comment: changed ProjectAddForm component
    
    * ESOLIDAR-421
    
    * ESOLIDAR-421 new version 1.3.9
    
    Co-authored-by: Joel Calheiros <joelcalheiros@gmail.com>

---
+++ src/components/inviteLink/InviteLink.md
@@ -0,0 +1,20 @@
+#### Import
+
+``` html
+import { InviteLink } from '@esolidar/toolkit';
+
+```
+
+#### Example
+
+``` jsx
+<InviteLink
+  inviteLinkText='You can invite your members to participate in your company members by sharing this link with them. Remember that who joins through this link will be associated to your company as an member.'
+  inviteLink='whitelabel_subdomain/lang/user/company-invite?invite_whitelabel=code'
+  copyLinkFunc={() => {}}
+  copied={false}
+  CopyLinkText='Copy link'
+  CopiedLinkText='Copied'
+/>
+```
+
---
+++ src/components/inviteLink/__tests__/InviteLink.test.js
@@ -0,0 +1,18 @@
+/* global expect */
+import React from 'react';
+import { shallow } from 'enzyme';
+import InviteLink from '../InviteLink';
+
+describe('InviteLink component', () => {
+  it('renders InviteLink correctly', () => {
+    const component = shallow(<InviteLink
+      inviteLinkText="You can invite your members..."
+      inviteLink="whitelabel_subdomain/lang/user/.."
+      copyLinkFunc={() => {}}
+      copied={false}
+      CopyLinkText="Copy link"
+      CopiedLinkText="Copied"
+    />);
+    expect(component).toHaveLength(1);
+  });
+});
