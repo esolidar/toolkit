84d59160d92a8718e8b54a2f29b5ac252c70f1db
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2020-05-28 15:44:39 +0100

    Esolidar 563 (#38)
    * ESOLIDAR-563 #comment feed components
    
    * ESOLIDAR-563 #comment CreatePost and Post small changes
    
    * ESOLIDAR-563 #comment add post to toolkit
    
    * ESOLIDAR-563 #comment add post to toolkit
    
    * ESOLIDAR-563 #comment add post to toolkit
    
    * ESOLIDAR-563 #comment posts and commentPost components
    
    * ESOLIDAR-563 #comment toolkit v1.6.9
    
    * ESOLIDAR-563 #comment toolkit v1.6.9
    
    * ESOLIDAR-563 #comment toolkit v1.7
    
    * ESOLIDAR-563 #comment toolkit v1.7.0
    
    * ESOLIDAR-563 #comment toolkit v1.7.1
    
    * ESOLIDAR-563 #comment toolkit v1.7.2
    
    * ESOLIDAR-563 #comment toolkit v1.7.2

---
+++ src/components/post/__tests__/Post.test.js
@@ -0,0 +1,50 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* global expect */
+import React from 'react';
+import { shallow } from 'enzyme';
+import Post from '../Post';
+
+const props = {
+  post: {
+    user: {
+      id: 1,
+      firstName: 'Maria',
+      lastName: 'Silva',
+      language: {
+        name: 'English',
+      },
+      thumbs: {
+        thumb: 'https://static.esolidar.com/frontend/logo/esolidar/logo.png',
+      },
+    },
+  },
+  user: {
+    id: 1,
+    firstName: 'Maria',
+    lastName: 'Silva',
+    language: {
+      name: 'English',
+    },
+    thumbs: {
+      thumb: 'https://static.esolidar.com/frontend/logo/esolidar/logo.png',
+    },
+  },
+  commentUpdated: {},
+  editComment: () => {},
+  deleteComment: () => {},
+  onSubmitReply: () => {},
+  addMessageKeyDown: () => {},
+  replyValue: '',
+  commentHereText: 'Comment here ...',
+  textareaOnChange: () => {},
+  errorsReply: [],
+  deletedComment: {},
+  onKeyDown: () => {},
+};
+
+describe('Post component', () => {
+  it('renders Post correctly', () => {
+    const component = shallow(<Post {...props} />);
+    expect(component).toHaveLength(1);
+  });
+});
