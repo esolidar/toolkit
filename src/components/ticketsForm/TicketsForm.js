e879219a58d79b94ff54928fc0f284d4ea97b76b
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2020-06-01 17:51:10 +0100

    Esolidar 563 (#44)
    * ESOLIDAR-563 #comment feed components
    
    * ESOLIDAR-563 #comment CreatePost and Post small changes
    
    * ESOLIDAR-563 #comment add post to toolkit
    
    * ESOLIDAR-563 #comment add post to toolkit
    
    * ESOLIDAR-563 #comment add post to toolkit
    
    * ESOLIDAR-563 #comment posts and commentPost components
    
    * ESOLIDAR-563 #comment toolkit v1.6.9
    
    * ESOLIDAR-563 #comment toolkit v1.6.9
    
    * ESOLIDAR-563 #comment toolkit v1.7
    
    * ESOLIDAR-563 #comment toolkit v1.7.0
    
    * ESOLIDAR-563 #comment toolkit v1.7.1
    
    * ESOLIDAR-563 #comment toolkit v1.7.2
    
    * ESOLIDAR-563 #comment toolkit v1.7.2
    
    * ESOLIDAR-563 #comment toolkit v1.7.3
    
    * ESOLIDAR-563 #comment toolkit v1.7.3
    
    * ESOLIDAR-563 #comment fix bug on Post component
    
    * ESOLIDAR-563 #comment fix bug on Post component
    
    * ESOLIDAR-563 #comment v1.8.2
    
    * ESOLIDAR-563 #comment v1.8.2
    
    * ESOLIDAR-563 #comment v1.8.3
    
    * ESOLIDAR-563 #comment v1.8.3
    
    * ESOLIDAR-563 #commment v1.9.0 tickets
    
    * ESOLIDAR-563 #commment v1.9.0 tickets
    
    * ESOLIDAR-563 #comment change tickets comments
    
    * ESOLIDAR-563 #comment change tickets comments
    
    * ESOLIDAR-563 #comment v1.9.1
    
    * ESOLIDAR-563 #comment v1.9.3

--- src/components/ticketsForm/TicketsForm.js
+++ src/components/ticketsForm/TicketsForm.js
@@ -10,9 +10,9 @@ import SelectField from '../../elements/selectField/SelectField';
 import Button from '../button/Button';
 
 const TicketsForm = ({
+  errors,
   onSubmit,
   onChange,
-  errors,
   statusDefault,
   status,
   priorityDefault,
@@ -32,174 +32,193 @@ const TicketsForm = ({
   onChangeComment,
   addComment,
   showCommentForm,
-}) => (
-  <Row>
-    <Col sm={12}>
-      {!showCommentForm ? (
-        <Card className="ticketCard">
-          <Card.Body>
-            <Row className="p-3">
-              <Col md={12} className="company-box">
-                <Row>
-                  <Col sm={12}>
-                    <TextField
-                      label={intl.formatMessage({ id: 'project.tickets.title', defaultMessage: 'Title' })}
-                      type="text"
-                      onChange={onChange}
-                      error={errors.title}
-                      placeholder={intl.formatMessage({ id: 'project.tickets.titlePlaceholder', defaultMessage: 'Title' })}
-                      defaultValue={editTicket.title}
-                      field="title"
-                      required={true}
-                    />
-                  </Col>
-                  {!hideText && (
-                  <Col sm={12}>
-                    <div className="form-group">
-                      <TextareaField
-                        label={intl.formatMessage({ id: 'project.tickets.text', defaultMessage: 'Description' })}
-                        error={errors.text}
-                        placeholder={intl.formatMessage({ id: 'project.tickets.textPlaceholder', defaultMessage: 'Enter your issue here.' })}
+  uploadedFiles,
+}) => {
+  const renderUploadedFiles = (files) => {
+    if (files.length > 0) {
+      return files.map((file, index) => (
+        <div className="document-row float-left pl-3" key={index}>
+          <a href={file.file} className="download-file" rel="noopener noreferrer" target="_blank" title={decodeURI(file.name)}>
+            {decodeURI(file.name)}
+          </a>
+        </div>
+      ));
+    }
+  };
+
+  return (
+    <Row>
+      <Col sm={12}>
+        {!showCommentForm ? (
+          <Card className="ticketCard">
+            <Card.Body>
+              <Row className="p-3">
+                <Col md={12} className="company-box">
+                  <Row>
+                    <Col sm={12}>
+                      <TextField
+                        label={intl.formatMessage({ id: 'project.tickets.title', defaultMessage: 'Title' })}
+                        type="text"
                         onChange={onChange}
-                        field="text"
-                        defaultValue={editTicket.text}
-                        message=""
-                        maxLength={100}
+                        error={errors.title}
+                        placeholder={intl.formatMessage({ id: 'project.tickets.titlePlaceholder', defaultMessage: 'Title' })}
+                        defaultValue={editTicket.title}
+                        field="title"
                         required={true}
                       />
-                    </div>
-                  </Col>
-                  )}
-                  <Col sm={6}>
-                    <SelectField
-                      defaultValue={statusDefault}
-                      onChange={onChange}
-                      field="status"
-                      label={intl.formatMessage({ id: 'project.tickets.status', defaultMessage: 'Status' })}
-                      options={status}
-                      error={errors.status}
-                      required={true}
-                    />
-                  </Col>
-                  <Col sm={6}>
-                    <SelectField
-                      defaultValue={priorityDefault}
-                      onChange={onChange}
-                      field="priority"
-                      label={intl.formatMessage({ id: 'project.tickets.priority', defaultMessage: 'Priority' })}
-                      options={priority}
-                      error={errors.priority}
-                      required={true}
-                    />
-                  </Col>
-                  {showAddFilesButtton && (
-                  <Col sm={12}>
-                    <Button
-                      extraClass="dark-full"
-                      onClick={openModalFiles}
-                      text={intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
-                    />
-                  </Col>
-                  )}
-
-                  <Col sm={12}>
-                    <Row>
-                      <Col sm={12} className="text-right">
-                        <Button
-                          extraClass="success-full"
-                          onClick={onSubmit}
-                          disabled={isLoading}
-                          text={intl.formatMessage({ id: 'Company.department.save', defaultMessage: 'Save' })}
-                        />
+                    </Col>
+                    {!hideText && (
+                      <Col sm={12}>
+                        <div className="form-group">
+                          <TextareaField
+                            label={intl.formatMessage({ id: 'project.tickets.text', defaultMessage: 'Description' })}
+                            error={errors.text}
+                            placeholder={intl.formatMessage({ id: 'project.tickets.textPlaceholder', defaultMessage: 'Enter your issue here.' })}
+                            onChange={onChange}
+                            field="text"
+                            defaultValue={editTicket.text}
+                            message=""
+                            maxLength={100}
+                            required={true}
+                          />
+                        </div>
                       </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      ) : (
-        <Card className="ticketCard">
-          <Card.Body>
-            <Row>
-              <Col sm={12}>
-                <TextareaField
-                  label={intl.formatMessage({ id: 'project.tickets.text', defaultMessage: 'Description' })}
-                  error={errors.text}
-                  placeholder={intl.formatMessage({ id: 'project.tickets.textPlaceholder', defaultMessage: 'Enter your issue here.' })}
-                  onChange={onChangeComment}
-                  field="text"
-                  message=""
-                  maxLength={100}
-                  required={true}
-                />
-              </Col>
-              <Col sm={12} className="mt-3 text-left">
-                <Button
-                  extraClass="dark-full"
-                  onClick={toggleModalFiles}
-                  text={intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
-                />
-              </Col>
-              <Col sm={12}>
-                <Row>
-                  <Col sm={12} className="text-right">
-                    <Button
-                      extraClass="dark-full mr-3"
-                      onClick={addComment}
-                      text={intl.formatMessage({ id: 'Company.department.cancel', defaultMessage: 'Cancel' })}
-                      disabled={isLoading}
-                    />
-                    <Button
-                      extraClass="success-full"
-                      onClick={onSubmitComment}
-                      text={intl.formatMessage({ id: 'Company.department.save', defaultMessage: 'Save' })}
-                      disabled={isLoading}
-                    />
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      )}
-    </Col>
-    <Modal show={showModalFiles} onHide={toggleModalFiles} className="md-import-employees">
-      <Modal.Header closeButton>
-        <Modal.Title>
-          {intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="row header-import select-employees-list">
-          <Col sm={12} className="border-top">
-            <Button
-              extraClass="success-full"
-              onClick={toggleModalFiles}
-              text={intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
-            />
-          </Col>
-          <Col sm={12} className="checkbox-inline">
-            <div className="search-modal-files">
-              <TextField
-                type="text"
-                className="search-files"
-                onChange={searchFiles}
-                placeholder={intl.formatMessage({ id: 'project.tickets.searchFiles', defaultMessage: 'Pesquisar por nome' })}
-                defaultValue={search}
-                field="search"
+                    )}
+                    <Col sm={6}>
+                      <SelectField
+                        defaultValue={statusDefault}
+                        onChange={onChange}
+                        field="status"
+                        label={intl.formatMessage({ id: 'project.tickets.status', defaultMessage: 'Status' })}
+                        options={status}
+                        error={errors.status}
+                        required={true}
+                      />
+                    </Col>
+                    <Col sm={6}>
+                      <SelectField
+                        defaultValue={priorityDefault}
+                        onChange={onChange}
+                        field="priority"
+                        label={intl.formatMessage({ id: 'project.tickets.priority', defaultMessage: 'Priority' })}
+                        options={priority}
+                        error={errors.priority}
+                        required={true}
+                      />
+                    </Col>
+                    {showAddFilesButtton && (
+                      <Col sm={12}>
+                        <Col sm={12}>
+                          <Button
+                            extraClass="dark-full float-left"
+                            onClick={openModalFiles}
+                            text={intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
+                          />
+                          {renderUploadedFiles(uploadedFiles)}
+                        </Col>
+                      </Col>
+                    )}
+                    <Col sm={12}>
+                      <Row>
+                        <Col sm={12} className="text-right">
+                          <Button
+                            extraClass="success-full"
+                            onClick={onSubmit}
+                            disabled={isLoading}
+                            text={intl.formatMessage({ id: 'Company.department.save', defaultMessage: 'Save' })}
+                          />
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        ) : (
+          <Card className="ticketCard">
+            <Card.Body>
+              <Row>
+                <Col sm={12}>
+                  <TextareaField
+                    label={intl.formatMessage({ id: 'project.tickets.text', defaultMessage: 'Description' })}
+                    error={errors.text}
+                    placeholder={intl.formatMessage({ id: 'project.tickets.textPlaceholder', defaultMessage: 'Enter your issue here.' })}
+                    onChange={onChangeComment}
+                    field="text"
+                    message=""
+                    maxLength={100}
+                    required={true}
+                  />
+                </Col>
+                <Col sm={12} className="mt-3 text-left">
+                  <Button
+                    extraClass="dark-full float-left"
+                    onClick={toggleModalFiles}
+                    text={intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
+                  />
+                  {renderUploadedFiles(uploadedFiles)}
+                </Col>
+                <Col sm={12}>
+                  <Row>
+                    <Col sm={12} className="text-right">
+                      <Button
+                        extraClass="dark-full mr-3"
+                        onClick={addComment}
+                        text={intl.formatMessage({ id: 'Company.department.cancel', defaultMessage: 'Cancel' })}
+                        disabled={isLoading}
+                      />
+                      <Button
+                        extraClass="success-full"
+                        onClick={onSubmitComment}
+                        text={intl.formatMessage({ id: 'Company.department.save', defaultMessage: 'Save' })}
+                        disabled={isLoading}
+                      />
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        )}
+      </Col>
+      <Modal show={showModalFiles} onHide={toggleModalFiles} className="md-import-employees">
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row header-import select-employees-list">
+            <Col sm={12} className="border-top">
+              <Button
+                extraClass="success-full"
+                onClick={toggleModalFiles}
+                text={intl.formatMessage({ id: 'project.tickets.addFiles', defaultMessage: 'Add Files' })}
               />
-            </div>
-          </Col>
-        </div>
-        <div className="files-list checkbox-inline">
-          {renderFilesList()}
-        </div>
-      </Modal.Body>
-    </Modal>
-  </Row>
-);
+            </Col>
+            <Col sm={12} className="checkbox-inline">
+              <div className="search-modal-files">
+                <TextField
+                  type="text"
+                  className="search-files"
+                  onChange={searchFiles}
+                  placeholder={intl.formatMessage({ id: 'project.tickets.searchFiles', defaultMessage: 'Pesquisar por nome' })}
+                  defaultValue={search}
+                  field="search"
+                />
+              </div>
+            </Col>
+          </div>
+          <div className="files-list checkbox-inline">
+            {renderFilesList()}
+          </div>
+        </Modal.Body>
+      </Modal>
+    </Row>
+  );
+};
+
 
 TicketsForm.propTypes = {
   onSubmit: PropTypes.func,
@@ -227,6 +246,7 @@ TicketsForm.propTypes = {
   onChangeComment: PropTypes.func,
   addComment: PropTypes.func,
   showCommentForm: PropTypes.bool,
+  uploadedFiles: PropTypes.array,
 };
 
 export default injectIntl(TicketsForm);
