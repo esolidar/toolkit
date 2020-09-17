fae6925b0ff6974de328f76cd2866e56ec29d714
Joel Calheiros <joel@esolidar.com>
2020-09-17 11:18:08 +0100

    Feature/esolidar 1203 (#77)
    * ESOLIDAR-1203 #comment added edit button to project thumb component
    
    * updated package and styleguide versions
    
    * ESOLIDAR-1203 #comment added tests
    
    * ESOLIDAR-1203 #comment added tests

--- src/components/projectThumb/__tests__/ProjectThumb.test.js
+++ src/components/projectThumb/__tests__/ProjectThumb.test.js
@@ -87,6 +87,40 @@ const propsWithMoreOds = {
   status: 'Em revisão',
 };
 
+const propsPending = {
+  project: {
+    whitelabel_id: 1,
+    category_id: 1,
+    user_id: 1,
+    user: {
+      name: 'Joel Calheiros',
+      thumbs: {
+        thumb: 'https://esolidar-production-uploads.s3.eu-west-1.amazonaws.com/users/9/9-THUMB.jpg',
+      },
+    },
+    ods: [
+      {
+        id: 1,
+      },
+      {
+        id: 2,
+      },
+    ],
+    cover: 'feed/3f4b8396-c5bd-4c09-bb1f-f0738036e998-DETAIL.jpg',
+    title: 'Criação de um canil comunitário para o Bairro do Aleixo',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    form: '{}',
+    status: 'PENDING',
+    uuid: '123',
+    images: [],
+  },
+  serverlessResizeImage: 'https://image.testesolidar.com',
+  lang: 'pt',
+  cols: 3,
+  showStatus: true,
+  status: 'Em revisão',
+};
+
 describe('ProjectThumb component', () => {
   it('renders ProjectThumb correctly', () => {
     const component = shallow(<ProjectThumb {...props} />);
@@ -102,5 +136,10 @@ describe('ProjectThumb component', () => {
     const component = shallow(<ProjectThumb {...propsWithMoreOds} />);
     expect(component.find('.ods').length).toBe(4);
     expect(component.find('.more-ods').length).toBe(1);
+  });
+
+  it('renders ProjectThumb edit button with PENDING stuts', () => {
+    const component = shallow(<ProjectThumb {...propsPending} />);
+    expect(component.find('.edit-button').length).toBe(1);
   });
 });
