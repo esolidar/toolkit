8bf98bda0e9e9bf1059d7469b08c7c3da648efcd
Pedro Guia <pedroguia@live.com.pt>
2020-11-05 18:14:57 +0000

    Feature/esolidar 1308 (#114)
    * ESOLIDAR-1308 #comment improved HtmlEditor component
    
    * updated package version to 1.19.1
    
    * ESOLIDAR-1308 #comment added tests to Dropdown component

--- src/components/htmlEditor/HtmlEditor.js
+++ src/components/htmlEditor/HtmlEditor.js
@@ -1,296 +1,116 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable react/no-string-refs */
-import React, { Component } from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
 import PropTypes from 'prop-types';
-import { Row, Col, Modal } from 'react-bootstrap';
-import { FormattedMessage } from 'react-intl';
+import React, { useState } from 'react';
 import {
-  AtomicBlockUtils, Editor, EditorState, RichUtils, convertFromHTML, ContentState,
+  EditorState, convertToRaw, convertFromRaw, ContentState, convertFromHTML,
 } from 'draft-js';
-import ColorPicker, { colorPickerPlugin } from 'draft-js-color-picker';
-import { stateToHTML } from 'draft-js-export-html';
-import Loading from '../loading/Loading';
-import Button from '../button/Button';
-import BlockStyleControls from './draft/BlockStyleControls';
-import InlineStyleControls from './draft/InlineStyleControls';
-import { mediaBlockRenderer } from './entities/mediaBlockRenderer';
-import Icon from '../icon/Icon';
+import { Editor } from '@pedroguia/react-draft-wysiwyg';
+import { injectIntl } from 'react-intl';
+import htmlToDraft from 'html-to-draftjs';
+import Dropdown from './Dropdown';
+import '@pedroguia/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-const styleMap = {
-  CODE: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 2,
-  },
+const HtmlEditor = ({
+  onFileUpload, initialContent, onChange, columns, changeColumns, intl,
+}) => {
+  const isContentHtmlString = typeof initialContent === 'string';
+
+  const getInitialRawContent = () => {
+    if (isContentHtmlString) {
+      const fixedHtml = initialContent.replaceAll('<figure>', '<p>').replaceAll('</figure>', '</p>');
+      const numberOfElements = convertFromHTML(fixedHtml).contentBlocks.length;
+      const blocksFromHTML = htmlToDraft(fixedHtml);
+      const content = ContentState.createFromBlockArray(
+        blocksFromHTML.contentBlocks,
+        blocksFromHTML.entityMap,
+      );
+
+      const rawContent = convertToRaw(content);
+      if (rawContent.blocks.length > numberOfElements && rawContent.blocks[0].text === '') rawContent.blocks.shift();
+      return rawContent;
+    }
+    return initialContent;
+  };
+
+  const [editorState, setEditorState] = useState(
+    initialContent
+      ? EditorState.createWithContent(convertFromRaw(getInitialRawContent()))
+      : EditorState.createEmpty(),
+  );
+
+  const handleFileUpload = (file) => new Promise(
+    (resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = async () => {
+        const imageUrl = await onFileUpload(reader.result);
+        const result = { data: { link: imageUrl } };
+        resolve(result);
+      };
+      reader.onerror = async () => {
+        const imageUrl = await onFileUpload(reader.result);
+        const error = { data: { link: imageUrl } };
+        reject(error);
+      };
+    },
+  );
+
+  const handleEditorStateChange = (editorState) => {
+    if (onChange) onChange(convertToRaw(editorState.getCurrentContent()));
+    setEditorState(editorState);
+  };
+
+  return (
+    <Editor
+      editorState={editorState}
+      onEditorStateChange={handleEditorStateChange}
+      localization={{
+        locale: 'pt',
+        translations: {
+          'components.controls.link.linkTitle': 'Texto',
+          'components.controls.link.linkTarget': 'Endereço URL',
+          'components.controls.link.linkTargetOption': 'Abrir novo separador',
+          'components.controls.link.link': 'Adicionar ligação',
+          'components.controls.link.unlink': 'Remover ligação',
+        },
+      }}
+      toolbar={{
+        options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'image', 'remove', 'history'],
+        inline: {
+          options: ['bold', 'italic', 'underline', 'strikethrough'],
+        },
+        link: {
+          defaultTargetOption: '_blank',
+        },
+        image: {
+          uploadCallback: handleFileUpload,
+          alt: { present: true, mandatory: false },
+          previewImage: true,
+        },
+      }}
+      toolbarCustomButtons={[<Dropdown title={intl.formatMessage({ id: 'columns', defaultMessage: 'Columns' })} options={[1, 2, 3]} value={columns} handleChange={changeColumns} />]}
+      stripPastedStyles={true}
+      spellCheck={true}
+    />
+  );
 };
-
-const presetColors = [
-  '#000000',
-  '#f44336',
-  '#e91e63',
-  '#9c27b0',
-  '#673ab7',
-  '#3f51b5',
-  '#2196f3',
-  '#03a9f4',
-  '#00bcd4',
-  '#009688',
-  '#4caf50',
-  '#8bc34a',
-  '#ff5722',
-  '#795548',
-  '#50646d',
-];
-
-class HtmlEditor extends Component {
-  constructor(props) {
-    super(props);
-    const { html } = this.props;
-
-    const blocksFromHTML = convertFromHTML(html);
-    const content = ContentState.createFromBlockArray(
-      blocksFromHTML.contentBlocks,
-      blocksFromHTML.entityMap,
-    );
-
-    this.state = {
-      editorState: html ? EditorState.createWithContent(content) : EditorState.createEmpty(),
-      isLoading: false,
-      showModal: false,
-    };
-
-    this.onChange = (editorState) => this.setState({ editorState });
-
-    this.handleKeyCommand = (command) => this._handleKeyCommand(command);
-    this.onTab = (e) => this._onTab(e);
-    this.toggleBlockType = (type) => this._toggleBlockType(type);
-    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
-    this.saveContent = this.saveContent.bind(this);
-
-    this.getEditorState = () => this.state.editorState;
-
-    this.picker = colorPickerPlugin(this.onChange, this.getEditorState);
-    this.onAddImage = this.onAddImage.bind(this);
-    this.onChangeInput = this.onChangeInput.bind(this);
-  }
-
-  componentDidUpdate(prevProps) {
-    const { imageUrl } = this.props;
-    if (prevProps.imageUrl !== imageUrl) {
-      if (imageUrl) {
-        this.onAddImage(imageUrl);
-      }
-    }
-  }
-
-  onChangeInput(e) {
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(e.target.files[0]);
-    reader.onload = () => {
-      this.toggleModal();
-      this.props.fileUpload(reader.result);
-    };
-  }
-
-  getBlockStyle(block) {
-    switch (block.getType()) {
-      case 'blockquote': return 'RichEditor-blockquote';
-      default: return null;
-    }
-  }
-
-  handleKeyCommand = (command) => {
-    const { editorState } = this.state;
-    const newState = RichUtils.handleKeyCommand(
-      editorState,
-      command,
-    );
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
-  };
-
-  onAddImage = (url) => {
-    const { editorState } = this.state;
-    const urlValue = url;
-    const contentState = editorState.getCurrentContent();
-    const contentStateWithEntity = contentState.createEntity(
-      'image',
-      'IMMUTABLE',
-      { src: urlValue },
-    );
-    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
-    const newEditorState = EditorState.set(
-      editorState,
-      { currentContent: contentStateWithEntity },
-      'create-entity',
-    );
-    this.setState({
-      editorState: AtomicBlockUtils.insertAtomicBlock(
-        newEditorState,
-        entityKey,
-        ' ',
-      ),
-    });
-  };
-
-  toggleModal = () => {
-    const { showModal } = this.state;
-    return this.setState({
-      showModal: !showModal,
-    });
-  };
-
-  saveContent() {
-    const { editorState } = this.state;
-    const { submitTextHtml } = this.props;
-    const inlineStyles = this.picker.exporter(editorState);
-    const content = stateToHTML(editorState.getCurrentContent(), { inlineStyles });
-    submitTextHtml(content);
-    this.setState({
-      isLoading: true,
-    });
-  }
-
-  _toggleInlineStyle(inlineStyle) {
-    const { editorState } = this.state;
-    this.onChange(RichUtils.toggleInlineStyle(
-      editorState,
-      inlineStyle,
-    ));
-  }
-
-  _toggleBlockType(blockType) {
-    const { editorState } = this.state;
-    this.onChange(RichUtils.toggleBlockType(
-      editorState,
-      blockType,
-    ));
-  }
-
-  _onTab(e) {
-    const { editorState } = this.state;
-    const maxDepth = 4;
-    this.onChange(RichUtils.onTab(e, editorState, maxDepth));
-  }
-
-  _handleKeyCommand(command) {
-    const { editorState } = this.state;
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return true;
-    }
-    return false;
-  }
-
-  render() {
-    const { editorState, isLoading, showModal } = this.state;
-    const {
-      columns, changeColumns, cancelTextHtml,
-    } = this.props;
-
-    // If the user changes block type before entering any text, we can
-    // either style the placeholder or hide it. Let's just hide it now.
-    let className = 'RichEditor-editor';
-    const contentState = editorState.getCurrentContent();
-    if (!contentState.hasText()) {
-      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
-        className += ' RichEditor-hidePlaceholder';
-      }
-    }
-
-    return (
-      <div className="htmlEditor">
-        {isLoading && (
-          <Loading css="loading-upload-image" />
-        )}
-        <div className="RichEditor-root">
-          <BlockStyleControls
-            editorState={editorState}
-            onToggle={this.toggleBlockType}
-          />
-          <InlineStyleControls
-            editorState={editorState}
-            columns={columns}
-            onToggle={this.toggleInlineStyle}
-            changeColumns={changeColumns}
-          />
-          <div className="RichEditor-controls colorPicker">
-            <ColorPicker
-              toggleColor={(color) => this.picker.addColor(color)}
-              presetColors={presetColors}
-              color={this.picker.currentColor(editorState) || 'rgba(0,0,0,1)'}
-            />
-            <button className="inline styleButton" type="button" onClick={this.toggleModal}>
-              <Icon iconClass="icon-image" />
-            </button>
-          </div>
-          <div className={className}>
-            <Editor
-              blockRendererFn={mediaBlockRenderer}
-              customStyleFn={this.picker.customStyleFn}
-              blockStyleFn={this.getBlockStyle}
-              customStyleMap={styleMap}
-              editorState={editorState}
-              handleKeyCommand={this.handleKeyCommand}
-              onChange={this.onChange}
-              onTab={this.onTab}
-              ref="editor"
-              spellCheck={true}
-            />
-          </div>
-        </div>
-        <Row>
-          <Col sm={6}>
-            <Button
-              extraClass="dark"
-              onClick={cancelTextHtml}
-              text={(
-                <FormattedMessage
-                  id="homepage.configuration.cancel"
-                  defaultMessage="Cancel"
-                />
-              )}
-            />
-          </Col>
-          <Col sm={6} className="text-right">
-            <Button
-              extraClass="success-full"
-              onClick={this.saveContent}
-              text={(
-                <FormattedMessage
-                  id="homepage.configuration.save"
-                  defaultMessage="Save"
-                />
-              )}
-            />
-          </Col>
-        </Row>
-        <Modal show={showModal} onHide={this.toggleModal} className="upload_file">
-          <Modal.Body>
-            <form>
-              <input type="file" onChange={this.onChangeInput} />
-            </form>
-          </Modal.Body>
-        </Modal>
-      </div>
-    );
-  }
-}
-
-export default HtmlEditor;
 
 HtmlEditor.propTypes = {
-  html: PropTypes.string.isRequired,
-  submitTextHtml: PropTypes.func.isRequired,
-  changeColumns: PropTypes.func.isRequired,
+  onFileUpload: PropTypes.func,
+  initialContent: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  onChange: PropTypes.func,
   columns: PropTypes.number,
-  cancelTextHtml: PropTypes.func.isRequired,
-  fileUpload: PropTypes.func.isRequired,
-  imageUrl: PropTypes.string,
+  changeColumns: PropTypes.func,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func,
+  }),
 };
+
+HtmlEditor.defaultValues = {
+  initialContent: '',
+  columns: 1,
+};
+
+export default injectIntl(HtmlEditor);
