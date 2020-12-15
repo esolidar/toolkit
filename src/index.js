d427f77a7bbf6ac4162e18d8bb553566f3dc0ce1
Joel Calheiros <joel@esolidar.com>
2020-12-15 18:43:14 +0000

    added isCompanyAdmin utils functions to return if user is company admin or owner

--- src/index.js
+++ src/index.js
@@ -66,5 +66,5 @@ export { default as SelectPerPage } from './elements/selectPerPage/SelectPerPage
 
 // Utils
 export {
-  getEmployeeName, isDefined, clone, firstElemOf, lastElemOf, addUrlParam, getUrlParam, removeUrlParam, filterUnique,
+  getEmployeeName, isDefined, clone, firstElemOf, lastElemOf, addUrlParam, getUrlParam, removeUrlParam, filterUnique, isCompanyAdmin,
 } from './utils';
--- src/utils/__tests__/index.test.js
+++ src/utils/__tests__/index.test.js
@@ -1,6 +1,6 @@
 /* global expect */
 import {
-  getEmployeeName, addUrlParam, removeUrlParam, getUrlParam, filterUnique,
+  getEmployeeName, addUrlParam, removeUrlParam, getUrlParam, filterUnique, isCompanyAdmin,
 } from '../index';
 
 describe('test utils functions', () => {
@@ -71,5 +71,42 @@ describe('test utils functions', () => {
 
     filterUnique(array, 'id');
     expect(filterUnique(array, 'id')).toEqual(expectArray);
+  });
+
+  test('should return isCompanyAdmin', () => {
+    const userWithWorkEmail = {
+      id: 1,
+      firstName: 'António',
+      lastName: 'Santos',
+      work_email: [
+        {
+          company_id: 1, name: 'António Santos', role: 'admin', department: null, user: null,
+        }, {
+          company_id: 2, name: 'João Paulo', role: 'owner', department: null, user: null,
+        }, {
+          company_id: 3, name: 'Manuel Silva', role: null, department: null, user: null,
+        },
+      ],
+    };
+
+    const userWithWorkEmailEmpty = {
+      id: 1,
+      firstName: 'António',
+      lastName: 'Santos',
+      work_email: [],
+    };
+
+    const withoutWithWorkEmail = {
+      id: 1,
+      firstName: 'António',
+      lastName: 'Santos',
+    };
+
+    expect(isCompanyAdmin(1, userWithWorkEmail)).toBe(true);
+    expect(isCompanyAdmin(2, userWithWorkEmail)).toBe(true);
+    expect(isCompanyAdmin(3, userWithWorkEmail)).toBe(false);
+    expect(isCompanyAdmin(4, userWithWorkEmail)).toBe(false);
+    expect(isCompanyAdmin(1, userWithWorkEmailEmpty)).toBe(false);
+    expect(isCompanyAdmin(1, withoutWithWorkEmail)).toBe(false);
   });
 });
