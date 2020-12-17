2988bfdd30157f61a6af991ef055f9f0f910a3bf
Joel Calheiros <joel@esolidar.com>
2020-12-17 18:33:08 +0000

    Added isValidURL function to validate url string

--- src/utils/index.js
+++ src/utils/index.js
@@ -45,3 +45,13 @@ export const isCompanyAdmin = (companyId, user) => {
   const isAdmin = findIndex(user.work_email, (o) => o.company_id === companyId && (o.role === 'admin' || o.role === 'owner'));
   return isAdmin >= 0;
 };
+
+export const isValidURL = (str) => {
+  const pattern = new RegExp('^(https?:\\/\\/)?' // protocol
+    + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // domain name
+    + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
+    + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port and path
+    + '(\\?[;&a-z\\d%_.~+=-]*)?' // query string
+    + '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
+};
