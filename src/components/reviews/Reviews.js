97c7d082e8b5a414cd69cf7602be50e99435ec6d
Pedro Guia <pedroguia@live.com.pt>
2020-12-22 10:01:31 +0000

    ESOLIDAR-1369 #comment refactored visual and logic of Reviews component, addded tests (#149)

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,172 +1,217 @@
-import React, { useState, useEffect } from 'react';
-import { Accordion, Card } from 'react-bootstrap';
+import React, { useMemo } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Rating from 'react-rating';
-import { filter } from 'lodash';
 import { FormattedMessage } from 'react-intl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 import TextareaField from '../../elements/textareaField/TextareaField';
 import Button from '../button/Button';
-import { getEmployeeName } from '../../utils';
+import { getEmployeeName, isDefined } from '../../utils';
 
 const Reviews = ({
-  defaultActiveKey,
-  reviews,
+  averageRate,
   companyId,
-  serverlessResizeImage,
-  noReviewsText,
-  texts,
+  errors,
   myUser,
-  onChange,
+  review,
+  onClickCancel,
+  onClickEdit,
   onChangeRate,
+  onChangeText,
+  otherReviewsList,
+  serverlessResizeImage,
+  showEditReviewForm,
   submitReview,
-  myRate,
-  myReview,
-  savedReview,
+  texts,
+  totalReviews,
+  userReview,
 }) => {
-  const [showMyReview, setShowMyReview] = useState(filter(reviews, ['user_id', myUser.id]).length);
+  const maxRate = '/5';
+  const noImagePlaceholder = 'https://static.esolidar.com/frontend/assets/no-image.png';
 
-  useEffect(() => {
-    if (savedReview) {
-      setShowMyReview(1);
-    }
-  }, [savedReview]);
+  const Average = () => (
+    useMemo(() => (
+      <div className="box overall-rating" data-testid="average-review">
+        <h3 className="title">
+          {texts.averageTitle}
+        </h3>
+        <div className="info">
+          <div className="numbers">
+            <span className="average">{averageRate}</span>
+            <span>{maxRate}</span>
+          </div>
+          <Rating
+            className="rating"
+            emptySymbol={<FontAwesomeIcon className="empty" icon={farStar} />}
+            fullSymbol={<FontAwesomeIcon className="full" icon={faStar} />}
+            readonly={true}
+            initialRating={averageRate}
+          />
+        </div>
+      </div>
+    )));
+
+  const OtherReviews = () => (
+    useMemo(() => (
+      <div className="box other-reviews">
+        <h3 className="title">
+          {texts.reviewTitle}
+        </h3>
+        {!otherReviewsList.length && (
+          <div className="no-reviews" data-testid="no-reviews">
+            {texts.noReviews}
+          </div>
+        )}
+        {!!otherReviewsList.length && otherReviewsList.map((review) => (
+          <div key={review.id} className="review" data-testid="other-review">
+            <div className="header">
+              <img className="img" src={review.user.s3_key ? `${serverlessResizeImage}/${review.user.s3_key}?width=40` : noImagePlaceholder} alt={getEmployeeName(companyId, review.user)} />
+              <div className="name">
+                {getEmployeeName(companyId, review.user)}
+              </div>
+              <Rating
+                className="rating"
+                emptySymbol={<FontAwesomeIcon className="empty" icon={farStar} />}
+                fullSymbol={<FontAwesomeIcon className="full" icon={faStar} />}
+                readonly={true}
+                initialRating={review.rate || 0}
+              />
+            </div>
+            {!!review.review && (
+              <div className="body">
+                <p>
+                  {review.review}
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    )));
 
   return (
     <div className="reviews">
-      {texts.intro && (
-        <p>
-          {texts.intro}
-        </p>
-      )}
-      <h3>
-        {texts.reviewTitle}
-      </h3>
-      {reviews.length === 0 && (
-        <div className="no-viviews">
-          {noReviewsText}
-        </div>
-      )}
-      {reviews.length > 0 && (
-        <Accordion defaultActiveKey={defaultActiveKey.toString()}>
-          {reviews.map((review, indx) => (
-            <Card key={review.id}>
-              <Accordion.Toggle as={Card.Header} eventKey={indx.toString()}>
-                <div className="header-user">
-                  <img src={review.user.s3_key ? `${serverlessResizeImage}/${review.user.s3_key}?width=40` : 'https://static.esolidar.com/frontend/assets/no-image.png'} alt={getEmployeeName(companyId, review.user)} />
-                  {getEmployeeName(companyId, review.user)}
-                  {myUser.id === review.user_id && (
-                    <button className="edit-review" type="button" onClick={() => setShowMyReview(0)}>
-                      {texts.editReviewButton}
-                    </button>
-                  )}
+      <Row>
+        {totalReviews > 0 && (
+          <Col md={12}>
+            <Average />
+          </Col>
+        )}
+        <Col md={12}>
+          <div className="box user-review" data-testid="user-review">
+            <h3 className="title">
+              {texts.myReview}
+            </h3>
+            <div className="review">
+              <div className="header">
+                <img className="img" src={myUser.s3_key ? `${serverlessResizeImage}/${myUser.s3_key}?width=40` : noImagePlaceholder} alt={getEmployeeName(companyId, myUser)} />
+                <div className="name">
+                  {getEmployeeName(companyId, myUser)}
+                </div>
+                {(!showEditReviewForm) && (
                   <Rating
                     className="rating"
-                    emptySymbol={(
-                      <img
-                        alt="Rating"
-                        src="https://static.esolidar.com/frontend/icons/star-empty.svg"
-                        className="icon"
-                      />
-                    )}
-                    fullSymbol={(
-                      <img
-                        alt="Rating"
-                        src="https://static.esolidar.com/frontend/icons/star-full.svg"
-                        className="icon"
-                      />
-                    )}
+                    emptySymbol={<FontAwesomeIcon className="empty" icon={farStar} />}
+                    fullSymbol={<FontAwesomeIcon className="full" icon={faStar} />}
                     readonly={true}
-                    initialRating={review.rate ? review.rate : 0}
-                  />
-                </div>
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey={indx.toString()}>
-                <Card.Body>
-                  {review.review}
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          ))}
-        </Accordion>
-      )}
-      {(showMyReview === 0 || reviews.length === 0) && (
-        <div className="add-review">
-          <h4>
-            {`${texts.myReviewTitle} ${getEmployeeName(companyId, myUser)}`}
-          </h4>
-          <div className="box">
-            <img className="my-user-thumb" src={myUser.s3_key ? `${serverlessResizeImage}/${myUser.s3_key}?width=40` : 'https://static.esolidar.com/frontend/assets/no-image.png'} alt={getEmployeeName(companyId, myUser)} />
-            <TextareaField
-              className="input"
-              field="review"
-              onChange={onChange}
-              defaultValue={myReview}
-              minRows={5}
-              placeholder={texts.writeReview}
-            />
-            <div className="rate">
-              <FormattedMessage
-                id="projects.review.rate"
-                defaultMessage="Review this project"
-              />
-              <Rating
-                className="rating"
-                emptySymbol={(
-                  <img
-                    alt="Rate"
-                    src="https://s3-eu-west-1.amazonaws.com/esolidar.com/frontend/icons/star-empty.svg"
-                    className="icon"
+                    initialRating={isDefined(userReview) ? userReview.rate : 0}
                   />
                 )}
-                fullSymbol={(
-                  <img
-                    alt="Rate"
-                    src="https://s3-eu-west-1.amazonaws.com/esolidar.com/frontend/icons/star-full.svg"
-                    className="icon"
-                  />
-                )}
-                initialRating={myRate}
-                onChange={(rate) => onChangeRate(rate)}
-              />
+              </div>
+              {(!showEditReviewForm) && (
+                <>
+                  <div className="body">
+                    {isDefined(userReview) && (
+                      <p>
+                        {userReview.review}
+                      </p>
+                    )}
+                    <Button extraClass="info" className="edit-button" onClick={onClickEdit} text={texts.editReviewButton} />
+                  </div>
+                </>
+              )}
+              {(showEditReviewForm) && (
+                <>
+                  <div className="form" data-testid="edit-form">
+                    <TextareaField
+                      className="input"
+                      field="review"
+                      onChange={onChangeText}
+                      value={review.review}
+                      minRows={5}
+                      placeholder={texts.writeReview}
+                    />
+                    <div className="rate">
+                      <FormattedMessage
+                        id="projects.review.rate"
+                        defaultMessage="Review this project"
+                      />
+                      <Rating
+                        className="rating"
+                        emptySymbol={<FontAwesomeIcon className="empty" icon={farStar} />}
+                        fullSymbol={<FontAwesomeIcon className="full" icon={faStar} />}
+                        initialRating={review.rate}
+                        onChange={(rate) => onChangeRate(rate)}
+                      />
+                    </div>
+                  </div>
+                  {isDefined(errors.rate) && (
+                    <div className="has-error" style={{ display: 'inline' }} data-testid="rate-error">
+                      <span className="help-block">{errors.rate}</span>
+                    </div>
+                  )}
+                  <div className="actions">
+                    {isDefined(userReview) && (<Button extraClass="dark" onClick={onClickCancel} text={texts.cancel} />)}
+                    <Button extraClass="success-full" onClick={submitReview} text={isDefined(userReview) ? texts.updateLabel : texts.saveLabel} />
+                  </div>
+                </>
+              )}
             </div>
           </div>
-          <div className="text-center">
-            <Button extraClass="success-full" onClick={submitReview} text={filter(reviews, ['user_id', myUser.id]).length === 1 ? texts.updateLabel : texts.saveLabel} />
-          </div>
-        </div>
-      )}
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12}>
+          <OtherReviews />
+        </Col>
+      </Row>
     </div>
   );
 };
 
 Reviews.propTypes = {
-  defaultActiveKey: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
+  averageRate: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   companyId: PropTypes.number.isRequired,
-  reviews: PropTypes.array.isRequired,
-  serverlessResizeImage: PropTypes.string.isRequired,
-  noReviewsText: PropTypes.string,
-  texts: PropTypes.object,
+  errors: PropTypes.object,
   myUser: PropTypes.object.isRequired,
+  review: PropTypes.object.isRequired,
+  onClickCancel: PropTypes.func.isRequired,
+  onClickEdit: PropTypes.func.isRequired,
   onChangeRate: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  otherReviewsList: PropTypes.array.isRequired,
+  serverlessResizeImage: PropTypes.string.isRequired,
+  showEditReviewForm: PropTypes.bool.isRequired,
   submitReview: PropTypes.func.isRequired,
-  myRate: PropTypes.number,
-  myReview: PropTypes.string,
-  savedReview: PropTypes.bool,
+  texts: PropTypes.object,
+  totalReviews: PropTypes.number.isRequired,
+  userReview: PropTypes.object.isRequired,
 };
 
 Reviews.defaultProps = {
-  defaultActiveKey: '',
   texts: {
+    averageTitle: 'Overall rating',
+    myReview: 'My review',
     reviewTitle: 'Reviews',
     myReviewTitle: 'Review from',
     editReviewButton: '(Edit)',
     writeReview: 'Write a review of this project',
-    updateLabel: 'Update my review',
-    saveLabel: 'Save my review',
+    updateLabel: 'Update',
+    saveLabel: 'Save',
+    cancel: 'Cancel',
+    noReviews: 'This project has no other reviews yet',
   },
 };
 
