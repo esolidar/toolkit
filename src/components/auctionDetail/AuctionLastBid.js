eef26cf27808734629ba4d40dc8ef8f11caf47ab
Rocha <amsrocha@outlook.com>
2021-01-22 18:37:23 +0000

    ESOLIDAR-1526 #comment sticky submit bid and project thumb in auction detail

--- src/components/auctionDetail/AuctionLastBid.js
+++ src/components/auctionDetail/AuctionLastBid.js
@@ -10,7 +10,6 @@ const AuctionLastBid = ({
   isEnded,
   isCommingSoon,
   handleClickBid,
-  isShowModal,
   error,
   translateMessage,
   intl,
@@ -22,128 +21,79 @@ const AuctionLastBid = ({
   const lastBid = auction.last_bid ? auction.last_bid.value : auction.bid_start;
 
   return (
-    <Col sm={12}>
-      <Row>
-        <Col className="box sticky-top mb-5" style={{ width: '100%', marginTop: '90px' }}>
-          <Row>
-            <Col>
-              <p className="control-label title-last-bid mb-2" id={`auction-last-bid-label-${auction.id}`} data-testid="title-last-bid" style={{ color: primaryColor }}>
-                {auction.last_bid ? (
-                  <FormattedMessage
-                    id="auction.detail.lastbid"
-                    defaultMessage="Last Bid"
-                  />
-                )
-                  : (
-                    <FormattedMessage
-                      id="auction.detail.startbid"
-                      defaultMessage="Start Bid"
-                    />
-                  )}
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col sm={12} className="txt-price-t" id={`auction-last-bid-value-${auction.id}`} data-testid="value-last-bid">
-              <FormattedNumber
-                value={lastBid}
-                style="currency"
-                currency={auction.currency.small}
-              />
-            </Col>
-          </Row>
-          {(!isEnded && !isCommingSoon) && (
-            <Row>
-              <Col sm={12} className="auction-content-label" data-testid="new-bid" style={{ color: primaryColor }}>
-                <FormattedMessage
-                  id="auction.detail.newBid"
-                  defaultMessage="New Bid"
-                />
-              </Col>
-              <Col sm={12} className={error && 'has-error'}>
-                <TextField
-                  dataTestId="bid-input"
-                  className="bid-input"
-                  type="number"
-                  onChange={valueBidTextField}
-                  error={error}
-                  value={inputBidValue}
-                  placeholder={
-                    intl.formatMessage(
-                      {
-                        id: 'auction.textfield.minValue',
-                        defaultMessage: 'Min. Value: {value}',
-                      },
-                      { value: minValue },
+    <Col sm={12} lg={10} className="header-bids mx-lg-auto">
+      {(!isEnded && !isCommingSoon) && (
+        <Row>
+          <Col className="box box-top sticky-top mb-5">
+            <Row className="d-flex justify-content-between">
+              <div className="d-none d-sm-block">
+                <Col>
+                  <p className="control-label title-last-bid mb-2" id={`auction-last-bid-label-${auction.id}`} data-testid="title-last-bid" style={{ color: primaryColor }}>
+                    {auction.last_bid ? (
+                      <FormattedMessage
+                        id="auction.detail.lastbid"
+                        defaultMessage="Last Bid"
+                      />
                     )
-                  }
-                  field="forCompanies"
-                />
-              </Col>
-              <Col sm={12}>
-                <Button
-                  dataTestId="button-bid"
-                  extraClass="success-full"
-                  text={translateMessage({ id: 'auction.button.bid', defaultMessage: 'Bid' })}
-                  onClick={() => handleClickBid(inputBidValue)}
-                />
-              </Col>
-              <Col sm={12} className="subscribe-auction text-center mt-4" data-testid="subscribe-link">
-                <button
-                  type="button"
-                  className="btn btn-link"
-                  onClick={() => isShowModal(true)}
-                >
-                  <FormattedMessage
-                    id="auction.detail.subscribeAuction"
-                    defaultMessage="Subscribe the auction"
+                      : (
+                        <FormattedMessage
+                          id="auction.detail.startbid"
+                          defaultMessage="Start Bid"
+                        />
+                      )}
+                  </p>
+                </Col>
+                <Col className="txt-price-t" id={`auction-last-bid-value-${auction.id}`} data-testid="value-last-bid">
+                  <FormattedNumber
+                    value={lastBid}
+                    style="currency"
+                    currency={auction.currency.small}
                   />
-                </button>
-              </Col>
+                </Col>
+              </div>
+
+              <div className="d-flex">
+                <div>
+                  <Col className="auction-content-label text-top d-none d-sm-block" data-testid="new-bid" style={{ color: primaryColor }}>
+                    <FormattedMessage
+                      id="auction.detail.newBid"
+                      defaultMessage="New Bid"
+                    />
+                  </Col>
+                  <Col sm={12} className={error && 'has-error'}>
+                    <TextField
+                      dataTestId="bid-input"
+                      className="bid-input"
+                      type="number"
+                      onChange={valueBidTextField}
+                      error={error}
+                      value={inputBidValue}
+                      placeholder={
+                        intl.formatMessage(
+                          {
+                            id: 'auction.textfield.minValue',
+                            defaultMessage: 'Min. Value: {value}',
+                          },
+                          { value: minValue },
+                        )
+                      }
+                      field="forCompanies"
+                    />
+                  </Col>
+                </div>
+                <Col className="mr-sm-3">
+                  <Button
+                    dataTestId="button-bid"
+                    extraClass="success-full mt-sm-4"
+                    text={translateMessage({ id: 'auction.button.bid', defaultMessage: 'Bid' })}
+                    onClick={() => handleClickBid(inputBidValue)}
+                  />
+                </Col>
+              </div>
             </Row>
-          )}
-          {isEnded && (
-            <>
-              <Row>
-                <Col sm={12} className="auction-content-label" data-testid="label-ended" style={{ color: primaryColor }}>
-                  <FormattedMessage
-                    id="auction.detail.ended"
-                    defaultMessage="Ended"
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={12} className="end-auction" data-testid="label-ended-message">
-                  <FormattedMessage
-                    id="auction.detail.endedAuction"
-                    defaultMessage="This auction has ended."
-                  />
-                </Col>
-              </Row>
-            </>
-          )}
-          {isCommingSoon && (
-            <>
-              <Row>
-                <Col sm={12} className="auction-content-label" data-testid="label-ended">
-                  <FormattedMessage
-                    id="auction.detail.commingSoon"
-                    defaultMessage="Comming Soon"
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={12} className="end-auction" data-testid="label-ended-message">
-                  <FormattedMessage
-                    id="auction.detail.commingSoonAuction"
-                    defaultMessage="You can make bids when this auction starts."
-                  />
-                </Col>
-              </Row>
-            </>
-          )}
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      )}
     </Col>
   );
 };
@@ -161,7 +111,6 @@ AuctionLastBid.propTypes = {
   isEnded: PropTypes.bool,
   isCommingSoon: PropTypes.bool,
   handleClickBid: PropTypes.func,
-  isShowModal: PropTypes.func,
   error: PropTypes.string,
   translateMessage: PropTypes.func,
   intl: PropTypes.shape({
