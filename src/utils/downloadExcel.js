7b8a00a42d3a9720cdc0d57a771a8b3c23efae40
Rocha <amsrocha@outlook.com>
2021-02-22 09:25:16 +0000

    ESOLIDAR-1603 #comment multiple select contries download excel

--- src/utils/downloadExcel.js
+++ src/utils/downloadExcel.js
@@ -9,6 +9,15 @@ const downloadExcel = (translateMessage, data, columns, fileName, isDownloadFile
           item[col.apiFieldName] === 1
             ? translateMessage({ id: 'yes' })
             : translateMessage({ id: 'no' });
+      } else if (col.type === 'flag') {
+        if (item[col.apiFieldName] === 208)
+          obj[col.text] = translateMessage({ id: 'country.portugal' });
+        if (item[col.apiFieldName] === 150)
+          obj[col.text] = translateMessage({ id: 'country.brazil' });
+        if (item[col.apiFieldName] === 231)
+          return translateMessage({ id: 'country.unitedKingdom' });
+        if (item[col.apiFieldName] === 232)
+          obj[col.text] = translateMessage({ id: 'country.unitedStates' });
       } else {
         obj[col.text] = item[col.apiFieldName];
       }
