e995afa704bca0eb3bfb0dcb5cd617d86a8644a1
Miguel Rocha <amsrocha@outlook.com>
2021-03-02 17:04:20 +0000

    Feature/esolidar 1683 (#214)
    * ESOLIDAR-1683 #comment create component ErrorBoundary
    
    * ESOLIDAR-1683 #comment ErrorBoundary
    
    * ESOLIDAR-1683 #comment errorBoundary style
    
    * ESOLIDAR-1683 #comment error boundary
    
    * ESOLIDAR-1683 #comment error boundary install sentry/browser
    
    * ESOLIDAR-1683 #comment fix bug data-testid

---
+++ src/components/errorBoundary/__tests__/ErrorBoundary.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, waitFor, screen } from '@testing-library/react';
+import { IntlProvider } from 'react-intl';
+import ErrorBoundary from '../index';
+
+const props = {
+  className: '',
+  color: {
+    primaryColor: '#5AC3E1',
+  },
+  showError: true,
+};
+
+test('Error Boundary', async () => {
+  render(
+    <IntlProvider locale="en">
+      <ErrorBoundary {...props} />
+    </IntlProvider>
+  );
+  await waitFor(() => {
+    const errorTitle = screen.getByTestId('errorBoundary-title');
+    expect(errorTitle).toBeInTheDocument();
+    const errorLink = screen.getByTestId('errorBoundary-link');
+    expect(errorLink).toBeInTheDocument();
+    expect(errorLink).toHaveStyle('color: #5AC3E1');
+    const errorEmoji = screen.getByTestId('errorBoundary-emoji');
+    expect(errorEmoji).toHaveTextContent('⚠️');
+  });
+});
