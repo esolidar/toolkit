6d23cb7ec8a62fef060c3d4389054a5e0ccefc7e
Miguel Rocha <amsrocha@outlook.com>
2021-03-08 15:32:34 +0000

    Feature/esolidar 1657 (#221)
    * ESOLIDAR-1657 ESOLIDAR-1757 #comment component DatePicker
    
    * ESOLIDAR-1757 #comment component DatePicker
    
    * ESOLIDAR-1757 #comment add defaultprops and registerLocale
    
    * ESOLIDAR-1757 #comment refactory DatePicker
    
    * ESOLIDAR-1757 #comment DatePicker with tests
    
    * ESOLIDAR-1757 #comment DatePicker receive erros
    
    * ESOLIDAR-1757 #comment DatePicker Label
    
    * ESOLIDAR-1757 #comment DatePicker change placeholderText

---
+++ src/elements/datePicker/DatePicker.md
@@ -0,0 +1,21 @@
+#### Import
+
+``` html
+import { DatePicker } from '@esolidar/toolkit';
+```
+
+#### Example
+
+``` jsx
+<DatePicker
+  classnames={() => {}}
+  label="Start Date"
+  locale="en"
+  selected={new Date('2021-03-05T10:20:30Z')}
+  startDate={new Date('2021-03-05T10:20:30Z')}
+  endDate={new Date('2021-03-06T10:20:30Z')}
+  onChange={() => {}}
+  className="form-control"
+  errors=''
+/>
+```
---
+++ src/elements/datePicker/__tests__/datePicker.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { advanceTo } from 'jest-date-mock';
+import DatePicker from '../index';
+
+const changed = jest.fn();
+
+const props = {
+  locale: 'en',
+  selected: new Date('2021-03-05T10:20:30Z'),
+  selectsStart: true,
+  startDate: new Date('2021-03-05T10:20:30Z'),
+  endDate: new Date('2021-03-06T10:20:30Z'),
+  showTimeSelect: true,
+  onChange: changed,
+  className: 'form-control',
+  placeholderText: 'DD-MM-YYYY',
+  timeCaption: 'hour',
+  dateFormat: 'd-MM-yyyy h:mm aa',
+};
+
+describe('DatePicker component', () => {
+  it('renders DatePicker correctly', () => {
+    const wrapper = shallow(<DatePicker />);
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('renders DatePicker correctly with props', () => {
+    advanceTo(new Date(2021, 1, 3, 0, 0, 0));
+
+    const wrapper = shallow(<DatePicker {...props} />);
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('renders DatePicker without label', () => {
+    advanceTo(new Date(2021, 1, 3, 0, 0, 0));
+
+    const wrapper = shallow(<DatePicker {...props} />);
+    expect(wrapper.find('.control-label').length).toBe(0);
+  });
+
+  it('renders DatePicker with label', () => {
+    advanceTo(new Date(2021, 1, 3, 0, 0, 0));
+
+    const wrapper = shallow(<DatePicker {...props} label="Start Date" />);
+    expect(wrapper.find('.control-label').length).toBe(1);
+  });
+
+  it('renders DatePicker with error', () => {
+    advanceTo(new Date(2021, 1, 3, 0, 0, 0));
+
+    const wrapper = shallow(<DatePicker {...props} errors="required field" />);
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.find('.error-block').length).toBe(1);
+  });
+});
