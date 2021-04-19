e10379639e6d6738c5e47ed5e92758851bc47f51
joelcalheiros <joel@esolidar.com>
2021-04-19 18:22:04 +0000

    Deploying to gh-pages from @ esolidar/toolkit@37d502267e5082375e07d7a72f9003520de004cb 🚀

--- constants/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.cdnUploadsUrl = exports.cdnStaticUrl = void 0;
-var cdnStaticUrl = 'https://static.esolidar.com';
-exports.cdnStaticUrl = cdnStaticUrl;
-var cdnUploadsUrl = 'https://cdn.testesolidar.com';
-exports.cdnUploadsUrl = cdnUploadsUrl;
