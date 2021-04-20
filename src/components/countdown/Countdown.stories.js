b2957dc353ad0e78966f234b75765f10c51e42cc
Joel Calheiros <joel@esolidar.com>
2021-04-20 11:15:50 +0100

    Changed action (#281)
    * ESOLIDAR-1864 #comment changed github action to install lock dependencies
    
    * ESOLIDAR-1864 #comment changed github action to install lock dependencies
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * Merge branch 'master' into changed-action
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * ESOLIDAR-1864 #comment changed github action to create github pages and publish package
    
    * ESOLIDAR-1864 #comment action to generate github pages
    
    * ESOLIDAR-1864 #comment action to generate github pages
    
    * ESOLIDAR-1864 #comment action to generate github pages

--- src/components/countdown/Countdown.stories.js
+++ src/components/countdown/Countdown.stories.js
@@ -1,15 +1,50 @@
+import moment from 'moment-timezone';
 import Countdown from './Countdown';
 
 export default {
   title: 'Components/Countdown',
   component: Countdown,
+  argTypes: {
+    startDate: {
+      control: {
+        type: 'text',
+      },
+    },
+    endDate: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 };
 
 const Template = args => <Countdown {...args} />;
+const format = 'YYYY/MM/DD HH:mm:ss';
 
-export const Default = Template.bind({});
-Default.args = {
-  endDate: '2021-02-08 10:25:00',
-  startDate: '2023-02-08 10:00:00',
+export const Soon = Template.bind({});
+Soon.args = {
+  startDate: moment().add(2, 'd').format(format),
+  endDate: moment().add(3, 'd').format(format),
+  thumb: false,
+};
+
+export const Running = Template.bind({});
+Running.args = {
+  startDate: moment().subtract(1, 'd').format(format),
+  endDate: moment().add(1, 'd').format(format),
+  thumb: false,
+};
+
+export const HeightHoursLeft = Template.bind({});
+HeightHoursLeft.args = {
+  startDate: moment().subtract(1, 'd').format(format),
+  endDate: moment().add(2, 'h').format(format),
+  thumb: false,
+};
+
+export const Ended = Template.bind({});
+Ended.args = {
+  startDate: '2020-02-08 00:00:00',
+  endDate: '2020-04-20 00:00:00',
   thumb: false,
 };
