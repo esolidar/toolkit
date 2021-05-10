4e78d693456983e7fe628b35147cc5dcf5c4f05e
Rocha <amsrocha@outlook.com>
2021-05-10 10:04:03 +0100

    ESOLIDAR-1952 add more tests validadions

--- src/components/auctionAddForm/__tests__/validations.test.js
+++ src/components/auctionAddForm/__tests__/validations.test.js
@@ -6,9 +6,7 @@ const data = {
   video: '',
   isValidBankAccount: false,
   isMyProjet: false,
-  images: {
-    length: 0,
-  },
+  images: [],
   title: '',
   bid_start: '',
   bid_interval: '',
@@ -27,6 +25,29 @@ const data = {
   projectIds: '',
 };
 
+const filledData = {
+  ...data,
+  images: [
+    {
+      id: 1,
+    },
+  ],
+  title: 'Auction Title',
+  bid_start: '1',
+  bid_interval: '1',
+  bid_max_interval: '100',
+  dateLimit: '2021-05-09',
+  dateStart: '2021-05-07',
+  description: 'Auction Description',
+  showBrands: true,
+  brand_id: 1,
+  private: '0',
+  showInstitutions: true,
+  showProjects: false,
+  user_id: 19,
+  beneficiary: '',
+};
+
 const errors = {
   bid_interval: <FormattedMessage id="user.register.error.required" />,
   bid_max_interval: <FormattedMessage id="user.register.error.required" />,
@@ -39,31 +60,128 @@ const errors = {
   projectIds: <FormattedMessage id="user.register.error.required" />,
 };
 
-test('All validatios pass', () => {
-  const filledData = {
-    ...data,
-    images: {
-      length: 1,
-    },
-    title: 'Auction Title',
-    bid_start: '1',
-    bid_interval: '1',
-    bid_max_interval: '100',
-    dateLimit: '2021-05-09',
-    dateStart: '2021-05-07',
-    description: 'Auction Description',
-    showBrands: true,
-    brand_id: 1,
-    private: '0',
-    showInstitutions: true,
-    showProjects: false,
-    user_id: 19,
-    beneficiary: '',
-  };
-
+test('All validations pass', () => {
   expect(validations(filledData)).toEqual({
     errors: {},
     isValid: true,
+  });
+});
+
+test('Youtube field error', () => {
+  const youtubeData = {
+    ...filledData,
+    video: 'test',
+  };
+  const errorYoutube = {
+    video: <FormattedMessage id="user.register.error.video" />,
+  };
+
+  expect(validations(youtubeData)).toEqual({
+    errors: errorYoutube,
+    isValid: false,
+  });
+});
+
+test('Date Start and Date limit field error', () => {
+  const datesData = {
+    ...filledData,
+    isValidBankAccount: false,
+    isMyProjet: true,
+  };
+  const errorDates = {
+    bankAccount: <FormattedMessage id="user.register.error.bank.account" />,
+  };
+
+  expect(validations(datesData)).toEqual({
+    errors: errorDates,
+    isValid: false,
+  });
+});
+
+test('Date Start and Date limit field error', () => {
+  const datesData = {
+    ...filledData,
+    dateLimit: undefined,
+    dateStart: undefined,
+  };
+  const errorDates = {
+    dateLimit: <FormattedMessage id="user.register.error.required" />,
+    dateStart: <FormattedMessage id="user.register.error.required" />,
+  };
+
+  expect(validations(datesData)).toEqual({
+    errors: errorDates,
+    isValid: false,
+  });
+});
+
+test('Private code empty field error', () => {
+  const datesData = {
+    ...filledData,
+    private: '1',
+    private_code: '',
+  };
+  const errorPrivateCode = {
+    private_code: <FormattedMessage id="user.register.error.required" />,
+  };
+
+  expect(validations(datesData)).toEqual({
+    errors: errorPrivateCode,
+    isValid: false,
+  });
+});
+
+test('Private code field error', () => {
+  const datesData = {
+    ...filledData,
+    private: '1',
+    private_code: '0000',
+  };
+  const errorPrivateCode = {
+    private_code: <FormattedMessage id="user.register.error.length6characteres" />,
+  };
+
+  expect(validations(datesData)).toEqual({
+    errors: errorPrivateCode,
+    isValid: false,
+  });
+});
+
+test('Private code field error', () => {
+  const userIdData = {
+    ...filledData,
+    user_id: '',
+  };
+  const errorUserId = {
+    user_id: <FormattedMessage id="user.register.error.required" />,
+  };
+
+  expect(validations(userIdData)).toEqual({
+    errors: errorUserId,
+    isValid: false,
+  });
+});
+
+test('Without Projects field error', () => {
+  const userIdData = {
+    ...filledData,
+    showProjects: true,
+    beneficiary: 'Projects',
+    projectIds: [
+      {
+        id: 1,
+      },
+    ],
+  };
+
+  const errorProjectId = {
+    user_id: <FormattedMessage id="user.register.error.select.only.one" />,
+    projectIds: <FormattedMessage id="user.register.error.select.only.one" />,
+  };
+
+  expect(validations(userIdData)).toEqual({
+    errors: errorProjectId,
+    isValid: false,
   });
 });
 
