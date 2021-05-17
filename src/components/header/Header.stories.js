65569b3b3a362ffd073ed1d05206b726956068cd
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2021-05-17 13:06:14 +0100

    Release/fe 35 (#304)
    * release/FE-35 #add header menu
    
    * release/FE-35
    
    * release/FE-35

---
+++ src/components/header/Header.stories.js
@@ -0,0 +1,23 @@
+import Header from './Header';
+
+export default {
+  title: 'Components/Header',
+  component: Header,
+};
+
+const Template = args => <Header {...args} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  jest: ['Header.test.js'],
+};
+Default.args = {
+  isUserLogged: false,
+  esolidarMainUrl: 'https://www.esolidar.com/',
+  esolidarUrl: 'https://community.testesolidar.com/',
+  esolidarBlogUrl: 'https://impactosocial.esolidar.com/',
+  esolidarHelpUrl: 'https://www.help.esolidar.com/',
+  businessCompanyName: '',
+  dashboardUrl: '',
+  institutionUrl: '',
+};
