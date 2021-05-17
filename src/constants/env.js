a4e07aaa86f3ffce1f41455d1f1cdbb421fb396f
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-05-17 16:04:40 +0100

    FE-70 #comment redesigned footer component

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,2 +1,9 @@
 export const cdnStaticUrl = 'https://static.esolidar.com';
 export const cdnUploadsUrl = 'https://cdn.testesolidar.com';
+
+export const hubSpot = {
+  portalId: '8443374',
+  formIdPt: '09658fc2-d1ef-41dd-9142-6c2dda93356e',
+  formIdBr: '8c3cae2a-10f6-41b3-9584-a3d69053c936',
+  formIdEn: 'cc0d71d2-1356-42b8-8124-813a793776a1',
+};
