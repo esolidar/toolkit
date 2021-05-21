9349705aad98034b9c0c5e149c9a4b022205cf28
patricia-mps <patricia@esolidar.pt>
2021-05-21 10:59:50 +0100

    header.test.js add IntlProvider

--- src/components/header/__tests__/Header.test.js
+++ src/components/header/__tests__/Header.test.js
@@ -1,10 +1,10 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { IntlProvider } from 'react-intl';
 import Header from '../index';
 
 describe('Header component', () => {
   it('renders Header correctly', () => {
-    const component = shallow(
+    const component = mount(
       <IntlProvider locale="en">
         <Header
           communityLinks={[]}
@@ -17,7 +17,7 @@ describe('Header component', () => {
   });
 
   it('renders Header logged out user on eSolidar', () => {
-    const component = shallow(
+    const component = mount(
       <IntlProvider locale="en">
         <Header
           isUserLogged={false}
@@ -35,78 +35,82 @@ describe('Header component', () => {
         />
       </IntlProvider>
     )
-      .shallow()
-      .shallow();
-    console.log(component.debug());
+      .mount()
+      .mount();
     expect(component.find('img')).toHaveLength(2);
-    expect(component.find('NavLink')).toHaveLength(9);
+    expect(component.find('NavLink')).toHaveLength(11);
     expect(component.find('NavDropdown')).toHaveLength(2);
-    expect(component.find('DropdownItem')).toHaveLength(6);
   });
 
-  // it('renders Header logged company user on eSolidar', () => {
-  //   const component = shallow(
-  //     <Header
-  //       isUserLogged={true}
-  //       esolidarMainUrl="https://www.esolidar.com/"
-  //       esolidarUrl="https://community.testesolidar.com/"
-  //       esolidarBlogUrl=""
-  //       esolidarHelpUrl=""
-  //       businessCompanyName=""
-  //       dashboardUrl="/social-feed"
-  //       institutionUrl=""
-  //       lang="pt"
-  //       communityLinks={[]}
-  //       dashboardLinks={[]}
-  //       cdnStaticUrl="https://static.esolidar.com"
-  //     />
-  //   );
-  //   expect(component.find('img')).toHaveLength(2);
-  //   expect(component.find('NavLink')).toHaveLength(3);
-  //   expect(component.find('NavLink').first().props().href).toEqual('/social-feed');
-  // });
+  it('renders Header logged company user on eSolidar', () => {
+    const component = mount(
+      <IntlProvider locale="en">
+        <Header
+          isUserLogged={true}
+          esolidarMainUrl="https://www.esolidar.com/"
+          esolidarUrl="https://community.testesolidar.com/"
+          esolidarBlogUrl=""
+          esolidarHelpUrl=""
+          businessCompanyName=""
+          dashboardUrl="/social-feed"
+          institutionUrl=""
+          lang="pt"
+          communityLinks={[]}
+          dashboardLinks={[]}
+          cdnStaticUrl="https://static.esolidar.com"
+        />
+      </IntlProvider>
+    );
+    expect(component.find('img')).toHaveLength(2);
+    expect(component.find('NavLink')).toHaveLength(3);
+    expect(component.find('NavLink').first().props().href).toEqual('/social-feed');
+  });
 
-  // it('renders Header logged npo user on eSolidar', () => {
-  //   const component = shallow(
-  //     <Header
-  //       isUserLogged={true}
-  //       esolidarMainUrl="https://www.esolidar.com/"
-  //       esolidarUrl="https://community.testesolidar.com/"
-  //       esolidarBlogUrl=""
-  //       esolidarHelpUrl=""
-  //       businessCompanyName=""
-  //       dashboardUrl=""
-  //       institutionUrl="/npo/store/1600-npo-test"
-  //       lang="pt"
-  //       communityLinks={[]}
-  //       dashboardLinks={[]}
-  //       cdnStaticUrl="https://static.esolidar.com"
-  //     />
-  //   );
-  //   expect(component.find('img')).toHaveLength(2);
-  //   expect(component.find('NavLink')).toHaveLength(3);
-  //   expect(component.find('NavLink').first().props().href).toEqual('/npo/store/1600-npo-test');
-  // });
+  it('renders Header logged npo user on eSolidar', () => {
+    const component = mount(
+      <IntlProvider locale="en">
+        <Header
+          isUserLogged={true}
+          esolidarMainUrl="https://www.esolidar.com/"
+          esolidarUrl="https://community.testesolidar.com/"
+          esolidarBlogUrl=""
+          esolidarHelpUrl=""
+          businessCompanyName=""
+          dashboardUrl=""
+          institutionUrl="/npo/store/1600-npo-test"
+          lang="pt"
+          communityLinks={[]}
+          dashboardLinks={[]}
+          cdnStaticUrl="https://static.esolidar.com"
+        />
+      </IntlProvider>
+    );
+    expect(component.find('img')).toHaveLength(2);
+    expect(component.find('NavLink')).toHaveLength(3);
+    expect(component.find('NavLink').first().props().href).toEqual('/npo/store/1600-npo-test');
+  });
 
-  // it('renders Header logged user on Business', () => {
-  //   const component = shallow(
-  //     <Header
-  //       isUserLogged={true}
-  //       esolidarMainUrl="https://www.esolidar.com/"
-  //       esolidarUrl="https://community.testesolidar.com/"
-  //       esolidarBlogUrl=""
-  //       esolidarHelpUrl=""
-  //       businessCompanyName="CompanyName"
-  //       dashboardUrl=""
-  //       institutionUrl=""
-  //       lang="pt"
-  //       communityLinks={[]}
-  //       dashboardLinks={[]}
-  //       cdnStaticUrl="https://static.esolidar.com"
-  //     />
-  //   );
-  //   expect(component.find('img')).toHaveLength(1);
-  //   expect(component.find('NavLink')).toHaveLength(3);
-  //   expect(component.find('NavLink').first().text()).toMatch('CompanyName');
-  // });
+  it('renders Header logged user on Business', () => {
+    const component = mount(
+      <IntlProvider locale="en">
+        <Header
+          isUserLogged={true}
+          esolidarMainUrl="https://www.esolidar.com/"
+          esolidarUrl="https://community.testesolidar.com/"
+          esolidarBlogUrl=""
+          esolidarHelpUrl=""
+          businessCompanyName="CompanyName"
+          dashboardUrl=""
+          institutionUrl=""
+          lang="pt"
+          communityLinks={[]}
+          dashboardLinks={[]}
+          cdnStaticUrl="https://static.esolidar.com"
+        />
+      </IntlProvider>
+    );
+    expect(component.find('img')).toHaveLength(1);
+    expect(component.find('NavLink')).toHaveLength(3);
+    expect(component.find('NavLink').first().text()).toMatch('CompanyName');
+  });
 });
