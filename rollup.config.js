9dc5024004368fdb51c66ba13bdd96529894afa3
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-05-27 19:13:05 +0100

    FE-111 #comment added code splitting to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,29 +4,28 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
 import scss from 'rollup-plugin-scss';
+import bundleScss from 'rollup-plugin-bundle-scss';
 import scssVariable from 'rollup-plugin-sass-variables';
 import json from '@rollup/plugin-json';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { terser } from 'rollup-plugin-terser';
+
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 
-const packageJson = require('./package.json');
+// const packageJson = require('./package.json');
 
 const extensions = [...DEFAULT_EXTENSIONS, '.ts', '.tsx'];
 
 export default {
-  input: 'src/index.ts',
+  input: ['src/index.ts', 'src/components/userMenu/index.ts', 'src/components/footer/index.js'],
   output: [
     {
-      file: packageJson.main,
+      dir: 'build',
       format: 'cjs',
       sourcemap: true,
     },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
   ],
+  preserveModules: true,
   plugins: [
     peerDepsExternal(),
     babel({
@@ -42,10 +41,14 @@ export default {
     }),
     resolve({ extensions }),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
     json(),
-    scss(),
+    bundleScss(),
+    // scss(),
     scssVariable(),
+    visualizer({
+      filename: 'bundle-analysis.html',
+    }),
+    typescript({ useTsconfigDeclarationDir: true }),
     terser(),
   ],
 };
