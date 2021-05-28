8740c603b10cd9f1872c23f410954d780e895cc6
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-05-28 11:17:35 +0100

    FE-128 #comment improved slugify parameters

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,10 +1,10 @@
 import slg from 'slugify';
 
-const slugify = v =>
-  slg(v, {
-    replacement: '-',
-    remove: /[?$*_+~./,()'"!\-:@]/g,
-    lower: true,
+const slugify = (value, replacement = '-', remove = /[?$*_+~./,()'"!\-:@]/g, lower = true) =>
+  slg(value, {
+    replacement,
+    remove,
+    lower,
   });
 
 export default slugify;
