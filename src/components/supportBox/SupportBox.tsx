a9b10694d82ac17ee372b710889b1b297a111e4f
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2021-06-29 12:40:37 +0100

    Feature/fe 207 (#364)
    * FE-207 #add new prop to SupportBox component
    
    * FE-207
    
    * FE-207
    
    * FE-207
    
    * FE-207 #add supportBox to auctionDetail
    
    * FE-207 change btn colors

--- src/components/supportBox/SupportBox.tsx
+++ src/components/supportBox/SupportBox.tsx
@@ -19,14 +19,13 @@ const SupportBox: FC<Props> = ({ campaign, communityUrl = '/' }: Props): JSX.Ele
       <div className="recipient-btn">
         <Button
           className="support-btn"
-          extraClass="info"
           href={`${communityUrl}npo/detail/${campaign.institution.id}-${slugify(
             campaign.institution.name
           )}`}
           text={intl.formatMessage({
             id: 'crowdfunding.crowdfunding.institution.helps',
           })}
-          target={communityUrl ? '_blank' : '_self'}
+          target="_blank"
         />
       </div>
     </section>
