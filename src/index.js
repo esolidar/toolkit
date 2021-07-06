08ba1a6899b89abbc3824ec1afbb257f83a9b31d
Rocha <amsrocha@outlook.com>
2021-07-06 09:54:45 +0100

    ENG-130 export component PasswordField

--- src/index.js
+++ src/index.js
@@ -66,6 +66,7 @@ export { default as ErrorBoundary } from './components/errorBoundary';
 export { default as supportedSection } from './components/supportedSection';
 export { default as UserMenu } from './components/userMenu';
 export { default as SupportBox } from './components/supportBox';
+export { default as PasswordField } from './components/passwordField';
 
 // Elements
 export { default as Button } from './elements/button';
