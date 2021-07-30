88d815b037ea5f72dc17b9591ab96e535aaddd58
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-07-30 12:09:18 +0100

    ENG-212 #comment Fix: fixed test-utils file extension and types

--- __customQueries__/test-utils.js
+++ __customQueries__/test-utils.js
@@ -1,9 +1,8 @@
-import { render, queries, RenderOptions } from '@testing-library/react';
+import { render, queries } from '@testing-library/react';
 import * as queryById from './queryById';
 import * as queryByClass from './queryByClass';
-import { ReactElement } from 'react';
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) =>
+const customRender = (ui, options) =>
   render(ui, { queries: { ...queries, ...queryByClass, ...queryById }, ...options });
 
 export * from '@testing-library/react';
