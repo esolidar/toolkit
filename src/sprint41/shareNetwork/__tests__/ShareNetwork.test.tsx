f24e8852e9ac700ea32f966ed5d903e56b3a797d
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2021-08-03 08:45:42 +0100

    ENG-138 #comment Drop: shareNetwork remove external packages (#395)
    * ENG-138 #comment Add: shareNetwork new component
    
    * ENG-138 #comment Add: shareNetwork new component typescript
    
    * ENG-138 #comment Add: shareNetwork new component new scss file
    
    * ENG-138 #comment Drop: remove external packages from new shareNetwork component
    
    * ENG-138 #comment Add:shareNetwork rel to target blank links

--- src/sprint41/shareNetwork/__tests__/ShareNetwork.test.tsx
+++ src/sprint41/shareNetwork/__tests__/ShareNetwork.test.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { composeStory } from '@storybook/testing-react';
+import translation from '@esolidar/i18n/projects/toolkit/en';
+import { IntlProvider } from 'react-intl';
 import { render } from '../../../../__customQueries__/test-utils';
 import Meta, { Default as DefaultStory } from '../ShareNetwork.stories';
 
 const Default = composeStory(DefaultStory, Meta);
 
 it('renders ShareNetwork default', () => {
-  const { getByLabelText, getAllByClass } = render(<Default />);
+  const { getByLabelText, getAllByClass } = render(
+    <IntlProvider locale="en" messages={translation}>
+      <Default />
+    </IntlProvider>
+  );
 
   expect(getByLabelText('facebook')).toBeTruthy();
   expect(getByLabelText('twitter')).toBeTruthy();
   expect(getByLabelText('whatsapp')).toBeTruthy();
-  expect(getByLabelText('share-link')).toBeTruthy();
+  expect(getByLabelText('copyToClipboard')).toBeTruthy();
   expect(getAllByClass(/share-icon/)).toHaveLength(4);
 });
