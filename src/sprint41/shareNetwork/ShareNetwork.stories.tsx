cd79e60efd1ac40581ae53d8e78dc326859426aa
Rocha <amsrocha@outlook.com>
2021-08-04 14:13:36 +0100

    ENG-241 #comment Add: Add props to social Network

--- src/sprint41/shareNetwork/ShareNetwork.stories.tsx
+++ src/sprint41/shareNetwork/ShareNetwork.stories.tsx
@@ -17,9 +17,57 @@ const Template: Story<Props> = (args: Props) => (
 );
 
 export const Default = Template.bind({});
+export const NotShowFacebook: Story<Props> = Template.bind({});
+export const NotShowTwitter: Story<Props> = Template.bind({});
+export const NotShowWhatsapp: Story<Props> = Template.bind({});
+export const NotShowCopyToClipboard: Story<Props> = Template.bind({});
 
 Default.args = {
   title: 'Campanha Solidária de Crowdfunding',
+  showFacebook: true,
+  showTwitter: true,
+  showWhatsapp: true,
+  showCopyToClipboard: true,
+  windowLocationHref:
+    'https://community.testesolidar.com/pt/crowdfunding/detail/137-teste-campanha-real-3',
+};
+
+NotShowFacebook.args = {
+  title: 'Campanha Solidária de Crowdfunding',
+  showFacebook: false,
+  showTwitter: true,
+  showWhatsapp: true,
+  showCopyToClipboard: true,
+  windowLocationHref:
+    'https://community.testesolidar.com/pt/crowdfunding/detail/137-teste-campanha-real-3',
+};
+
+NotShowTwitter.args = {
+  title: 'Campanha Solidária de Crowdfunding',
+  showFacebook: true,
+  showTwitter: false,
+  showWhatsapp: true,
+  showCopyToClipboard: true,
+  windowLocationHref:
+    'https://community.testesolidar.com/pt/crowdfunding/detail/137-teste-campanha-real-3',
+};
+
+NotShowWhatsapp.args = {
+  title: 'Campanha Solidária de Crowdfunding',
+  showFacebook: true,
+  showTwitter: true,
+  showWhatsapp: false,
+  showCopyToClipboard: true,
+  windowLocationHref:
+    'https://community.testesolidar.com/pt/crowdfunding/detail/137-teste-campanha-real-3',
+};
+
+NotShowCopyToClipboard.args = {
+  title: 'Campanha Solidária de Crowdfunding',
+  showFacebook: true,
+  showTwitter: true,
+  showWhatsapp: true,
+  showCopyToClipboard: false,
   windowLocationHref:
     'https://community.testesolidar.com/pt/crowdfunding/detail/137-teste-campanha-real-3',
 };
