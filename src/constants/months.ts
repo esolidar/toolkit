c46c46f530cf2468457c75bfe0bbac4bcc945bcd
Joel Calheiros <joel@esolidar.com>
2021-08-10 09:16:46 +0100

    ENG-234 fixed br and pt month dates (#409)
    * ENG-234 #comment Add new countdown component to crowdfunding and auctions
    
    * Merge branch 'master' into feature-ENG-234
    
    * ENG-234 #comment Add new countdown component to dates and timers
    
    * ENG-234 #comment fixed br and pt month dates
    
    * updated package version

---
+++ src/constants/months.ts
@@ -0,0 +1,52 @@
+export const MONTHS = [
+  {
+    short: 'short.jan',
+    long: 'long.january',
+  },
+  {
+    short: 'short.feb',
+    long: 'long.february',
+  },
+  {
+    short: 'short.mar',
+    long: 'long.march',
+  },
+  {
+    short: 'short.apr',
+    long: 'long.april',
+  },
+  {
+    short: 'short.may',
+    long: 'long.may',
+  },
+  {
+    short: 'short.jun',
+    long: 'long.june',
+  },
+  {
+    short: 'short.jul',
+    long: 'long.july',
+  },
+  {
+    short: 'short.aug',
+    long: 'long.august',
+  },
+  {
+    short: 'short.sep',
+    long: 'long.september',
+  },
+  {
+    short: 'short.oct',
+    long: 'long.october',
+  },
+  {
+    short: 'short.nov',
+    long: 'long.november',
+  },
+  {
+    short: 'short.dec',
+    long: 'long.december',
+  },
+];
+
+export default MONTHS;
