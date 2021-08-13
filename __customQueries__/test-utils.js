17583be0a1ce07ac48112358cfe48fd3c4317c1b
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-08-13 16:33:20 +0100

    changed test-utils

--- __customQueries__/test-utils.js
+++ __customQueries__/test-utils.js
@@ -4,17 +4,23 @@ import { IntlProvider } from 'react-intl';
 import * as queryById from './queryById';
 import * as queryByClass from './queryByClass';
 
-const messages = Object.assign(
-  require('@esolidar/i18n/projects/toolkit/en')
-  // require('@esolidar/i18n/projects/admin/en'),
-  // require('@esolidar/i18n/projects/business/en'),
-  // require('@esolidar/i18n/projects/esolidar/en'),
-  // require('@esolidar/i18n/projects/whitelabel/en')
-);
+const toolkit = require('@esolidar/i18n/projects/toolkit/en');
+const admin = require('@esolidar/i18n/projects/admin/en');
+const business = require('@esolidar/i18n/projects/business/en');
+const esolidar = require('@esolidar/i18n/projects/esolidar/en');
+const whitelabel = require('@esolidar/i18n/projects/whitelabel/en');
+
+const messages = {
+  toolkit,
+  admin,
+  business,
+  esolidar,
+  whitelabel,
+};
 
 const customRender = (ui, options) =>
   render(
-    <IntlProvider locale="en" messages={messages}>
+    <IntlProvider locale="en" messages={messages[process.env.PUBLIC_TOOLKIT_i18N]}>
       {ui}
     </IntlProvider>,
     { queries: { ...queries, ...queryByClass, ...queryById }, ...options }
