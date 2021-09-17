44f2293d106d24f7a83f23efd3524547e9ad79ac
Rocha <amsrocha@outlook.com>
2021-09-17 14:33:35 +0100

    fix convertToMyCurrency

--- src/utils/convertToMyCurrency.js
+++ src/utils/convertToMyCurrency.js
@@ -4,7 +4,8 @@ import { FormattedNumber } from 'react-intl';
 const convertToMyCurrency = (value, currency) => {
   let convertedValue = value;
   const user = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : null;
-  const myCurrency = user && user.currency !== 'null' ? user.currency : currency;
+  const myCurrency =
+    user && user.currency !== 'null' && user.currency !== undefined ? user.currency : currency;
 
   if (myCurrency.id !== currency.id) {
     convertedValue = (value * currency.value) / myCurrency.value;
