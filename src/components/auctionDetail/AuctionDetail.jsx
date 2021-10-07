ab7ded63750ffb7221fd4dea49cd2266ac8da2b9
Rocha <amsrocha@outlook.com>
2021-10-07 13:46:45 +0100

    ENG-429 #comment Fix: Fixed translation success new bid

--- src/components/auctionDetail/AuctionDetail.jsx
+++ src/components/auctionDetail/AuctionDetail.jsx
@@ -278,7 +278,7 @@ const AuctionDetail = ({
           alertVisible: true,
           alertClass: 'success',
           message: 'Your bid was successful',
-          messageId: 'auctions.modal.success.newBid',
+          messageId: 'toolkit.auctions.modal.success.newBid',
         },
       });
     } else if (newBid.status === 400) {
