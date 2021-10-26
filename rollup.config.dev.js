bec67c001bf97eb8d1e30f7b87bfb5fa180dec1c
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-10-26 11:10:54 +0100

    ENG-572 #comment Add: added empty indexjs file

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -16,7 +16,6 @@ const excludedExtensions = ['.stories.js', '.stories.tsx', '.test.js', '.test.ts
 
 export default {
   input: [
-    'src/index.js',
     ...getFiles('src/components', extensions, excludedExtensions),
     ...getFiles('src/constants', extensions, excludedExtensions),
     ...getFiles('src/elements', extensions, excludedExtensions),
