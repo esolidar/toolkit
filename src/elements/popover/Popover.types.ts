0c2591a3d39f8ac6c80b3efdbd6408a8dfb0681f
patricia-mps <patricia@esolidar.pt>
2021-10-29 15:31:32 +0100

    ENG-566 #comment Remove: unused types

--- src/elements/popover/Popover.types.ts
+++
@@ -1,8 +0,0 @@
-import { OverlayTriggerProps } from 'react-bootstrap/OverlayTrigger';
-
-interface Props extends OverlayTriggerProps {
-  popoverHeaderChildren?: JSX.Element;
-  popoverBodyChildren: JSX.Element;
-}
-
-export default Props;
