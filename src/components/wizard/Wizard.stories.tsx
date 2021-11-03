1d89c11d673998b3df8b86ae11236c723338ed6c
Joel Calheiros <joel@esolidar.com>
2021-11-03 08:44:42 +0000

    ENG-590 #comment Add wizard component

---
+++ src/components/wizard/Wizard.stories.tsx
@@ -0,0 +1,105 @@
+/* eslint-disable no-alert */
+import { Story, Meta } from '@storybook/react';
+import Wizard from './Wizard';
+import Props from './Wizard.types';
+
+export default {
+  title: 'Components/Wizard',
+  component: Wizard,
+  parameters: {
+    jest: ['Card.test.tsx'],
+  },
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => (
+  <div>
+    <Wizard {...args} />
+  </div>
+);
+
+export const Open: Story<Props> = Template.bind({});
+export const Close: Story<Props> = Template.bind({});
+
+Open.args = {
+  showWizard: true,
+  closeWizard: () => {},
+  title: 'A title',
+  subtitle: 'Volunteering',
+  status: 'draft',
+  buttonDarkText: 'Save & Close',
+  buttonPrimaryText: 'Publish',
+  cdnStaticUrl: 'https://static.esolidar.com',
+  saved: false,
+  pages: [
+    {
+      title: 'General details',
+      status: 'done',
+      active: false,
+    },
+    {
+      title: 'Categories ',
+      status: 'not-done',
+      active: true,
+    },
+    {
+      title: 'Skills & Interests',
+      status: 'not-done',
+      active: false,
+    },
+    {
+      title: 'Application form',
+      status: 'not-done',
+      active: false,
+    },
+  ],
+  body: <div style={{ textAlign: 'center' }}>Body Content Here</div>,
+  disabledDarkButton: false,
+  disabledPrimaryButton: true,
+  handleClickBack: () => {},
+  handleClickNext: () => {},
+  totalPages: 4,
+  currentPage: 2,
+  disableClickNext: true,
+};
+
+Close.args = {
+  showWizard: false,
+  closeWizard: () => {},
+  title: 'A title',
+  subtitle: 'Volunteering',
+  status: 'draft',
+  buttonDarkText: 'Save & Close',
+  buttonPrimaryText: 'Publish',
+  cdnStaticUrl: 'https://static.esolidar.com',
+  saved: false,
+  pages: [
+    {
+      title: 'General details',
+      status: 'done',
+      active: false,
+    },
+    {
+      title: 'Categories ',
+      status: 'not-done',
+      active: true,
+    },
+    {
+      title: 'Skills & Interests',
+      status: 'not-done',
+      active: false,
+    },
+    {
+      title: 'Application form',
+      status: 'not-done',
+      active: false,
+    },
+  ],
+  body: <div style={{ textAlign: 'center' }}>Body Content Here</div>,
+  disabledDarkButton: false,
+  disabledPrimaryButton: true,
+  handleClickBack: () => {},
+  handleClickNext: () => {},
+  totalPages: 4,
+  currentPage: 2,
+  disableClickNext: true,
+};
---
+++ src/components/wizard/Wizard.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from 'react';
+import WizardHeader from './header';
+import WizardPaginator from './paginator';
+import WizardFooter from './footer';
+import Props from './Wizard.types';
+
+const Wizard: FC<Props> = ({
+  showWizard,
+  closeWizard,
+  title,
+  subtitle,
+  status,
+  buttonDarkText,
+  buttonPrimaryText,
+  cdnStaticUrl,
+  saved,
+  pages,
+  body,
+  handleDarkButton,
+  handlePrimaryButton,
+  disabledDarkButton,
+  disabledPrimaryButton,
+  handleClickBack,
+  handleClickNext,
+  totalPages,
+  currentPage,
+  disableClickNext,
+}: Props): JSX.Element => {
+  return (
+    <div className={`wizard ${showWizard ? 'open' : 'closed'} `}>
+      <WizardHeader
+        closeWizard={closeWizard}
+        title={title}
+        subtitle={subtitle}
+        status={status}
+        buttonDarkText={buttonDarkText}
+        buttonPrimaryText={buttonPrimaryText}
+        cdnStaticUrl={cdnStaticUrl}
+        saved={saved}
+        handleDarkButton={handleDarkButton}
+        handlePrimaryButton={handlePrimaryButton}
+        disabledDarkButton={disabledDarkButton}
+        disabledPrimaryButton={disabledPrimaryButton}
+      />
+      <div>
+        <WizardPaginator pages={pages} cdnStaticUrl={cdnStaticUrl} />
+      </div>
+      <div className="container wizard__body">{body}</div>
+      <div className="container">
+        <WizardFooter
+          handleClickBack={handleClickBack}
+          handleClickNext={handleClickNext}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          disableClickNext={disableClickNext}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Wizard;
