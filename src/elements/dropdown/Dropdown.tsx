59bf0d47df997d852d87638cad504c1fbbf57254
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-11-19 15:48:12 +0000

    ENG-438 #comment Fix: addded stop propagation in dropdown item click

--- src/elements/dropdown/Dropdown.tsx
+++ src/elements/dropdown/Dropdown.tsx
@@ -5,30 +5,37 @@ import Icon from '../../components/icon';
 import CustomToggle from './CustomToggle';
 import Props from './Dropdown.types';
 
-const Dropdown: FC<Props> = ({ toggleIcon, items }: Props): JSX.Element => (
-  <DropdownBTS className="esolidar-dropdown">
-    <DropdownBTS.Toggle as={CustomToggle} id="dropdown-custom-components">
-      <Button extraClass="ghost" type="icon" icon={<Icon iconClass={toggleIcon} />} />
-    </DropdownBTS.Toggle>
-    <DropdownBTS.Menu className="esolidar-dropdown__menu">
-      {items.map(item => {
-        if (item.show)
-          return (
-            <DropdownBTS.Item
-              key={item.id}
-              className="esolidar-dropdown__item"
-              href={item.href}
-              onClick={item.onClick}
-              disabled={item.disabled}
-            >
-              {item.leftIcon && <Icon iconClass={item.leftIcon} />}
-              <span className="esolidar-dropdown__item--text">{item.text}</span>
-              {item.rightIcon && <Icon iconClass={item.rightIcon} />}
-            </DropdownBTS.Item>
-          );
-      })}
-    </DropdownBTS.Menu>
-  </DropdownBTS>
-);
+const Dropdown: FC<Props> = ({ toggleIcon, items }: Props): JSX.Element => {
+  const handleClick = (e, item) => {
+    e.stopPropagation();
+    item.onClick();
+  };
+
+  return (
+    <DropdownBTS className="esolidar-dropdown">
+      <DropdownBTS.Toggle as={CustomToggle} id="dropdown-custom-components">
+        <Button extraClass="ghost" type="icon" icon={<Icon iconClass={toggleIcon} />} />
+      </DropdownBTS.Toggle>
+      <DropdownBTS.Menu className="esolidar-dropdown__menu">
+        {items.map(item => {
+          if (item.show)
+            return (
+              <DropdownBTS.Item
+                key={item.id}
+                className="esolidar-dropdown__item"
+                href={item.href}
+                onClick={e => handleClick(e, item)}
+                disabled={item.disabled}
+              >
+                {item.leftIcon && <Icon iconClass={item.leftIcon} />}
+                <span className="esolidar-dropdown__item--text">{item.text}</span>
+                {item.rightIcon && <Icon iconClass={item.rightIcon} />}
+              </DropdownBTS.Item>
+            );
+        })}
+      </DropdownBTS.Menu>
+    </DropdownBTS>
+  );
+};
 
 export default Dropdown;
