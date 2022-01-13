cc64bf89d4104a7bc9a0ece927e51923296c252d
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2022-01-13 12:44:19 +0000

    ENG-788 #comment Add: changed getEnvVar function

--- src/utils/getEnvVar/getEnvVar.test.js
+++ src/utils/getEnvVar/getEnvVar.test.js
@@ -2,5 +2,5 @@ import '@testing-library/jest-dom';
 import getEnvVar from '.';
 
 test('test getEnvVar', () => {
-  expect(getEnvVar('PUBLIC_CDN_UPLOADS_URL')).toEqual('https://cdn.testesolidar.com');
+  expect(getEnvVar('CDN_UPLOADS_URL')).toEqual('https://cdn.testesolidar.com');
 });
--- src/utils/getEnvVar/getEnvVar.ts
+++ src/utils/getEnvVar/getEnvVar.ts
@@ -1,8 +1,10 @@
 /**
  * Gets the value of an environment variable in both Next.js and React projects
- * @param {string} name - env variable name without NEXT_ prefix, example: PUBLIC_CDN_UPLOADS_URL
+ * @param {string} name environment variable name without 'NEXT_PUBLIC_' prefixes, ex: CDN_UPLOADS_URL
+ * @returns {string} environment variable value
  */
 
-const getEnvVar = name => process.env[`NEXT_${name}`] || process.env[name];
+const getEnvVar = (name: string): string =>
+  process.env[`NEXT_PUBLIC_${name.toLowerCase()}`] || process.env[`PUBLIC_${name.toUpperCase()}`];
 
 export default getEnvVar;
