db6882d933659c89127b666cdcbdeff46cb5e2c5
Joel Calheiros <joel@esolidar.com>
2022-01-25 09:35:26 +0000

    Feature eng 831 (#604)
    * ENG-831 #comment Changed breadcrumbs component and added convert dates func
    
    * Merge branch 'master' into feature-ENG-831
    
    * Merge branch 'master' into feature-ENG-831
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed tests

--- src/elements/breadcrumbs/Breadcrumbs.tsx
+++ src/elements/breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import classnames from 'classnames';
 import Icon from '../icon';
+import Tooltip from '../tooltip';
 import Props from './Breadcrumbs.types';
 
 const Breadcrumbs: FC<Props> = ({ breadcrumbs, style }: Props): JSX.Element => {
@@ -14,9 +15,16 @@ const Breadcrumbs: FC<Props> = ({ breadcrumbs, style }: Props): JSX.Element => {
             className={classnames('esolidar-breadcrumbs__button', {
               'disabled-item': breadcrumbs.length === i + 1,
             })}
-            disabled={breadcrumbs.length === i + 1}
           >
-            {item.title}
+            {item.title.length <= 20 ? (
+              item.title
+            ) : (
+              <Tooltip
+                placement="top"
+                tooltipBodyChild={<>{`${item.title.substring(0, 20)}...`}</>}
+                overlay={<>{item.title}</>}
+              />
+            )}
           </button>
           {breadcrumbs.length !== i + 1 && <Icon name="Dot" />}
         </React.Fragment>
