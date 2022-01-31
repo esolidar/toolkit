339a9b93da391283292ac97c4b19b10cfbedecdb
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2022-01-31 11:25:38 +0000

    ENG-846 #Add: added story large to icons

--- src/elements/icon/__tests__/Icon.test.js
+++ src/elements/icon/__tests__/Icon.test.js
@@ -5,11 +5,13 @@ import Meta, {
   Default as DefaultStory,
   Small as SmallStory,
   XSmall as XSmallStory,
+  Large as LArgeStory,
 } from '../Icon.stories';
 
 const Default = composeStory(DefaultStory, Meta);
 const Small = composeStory(SmallStory, Meta);
 const XSmall = composeStory(XSmallStory, Meta);
+const Large = composeStory(LArgeStory, Meta);
 
 it('renders Icon default', () => {
   const { getAllByClass } = render(<Default />);
@@ -25,6 +27,12 @@ it('renders Icon Small', () => {
 
 it('renders Icon XSmall', () => {
   const { getAllByClass } = render(<XSmall />);
+
+  expect(getAllByClass('icon-component')).toBeTruthy();
+});
+
+it('renders Icon Large', () => {
+  const { getAllByClass } = render(<Large />);
 
   expect(getAllByClass('icon-component')).toBeTruthy();
 });
