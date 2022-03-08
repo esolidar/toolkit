f11af96069b16c9848468a3fd4dde4bc0d700c7b
patricia-mps <patricia@esolidar.pt>
2022-03-08 10:51:15 +0000

    ENG-1030 #comment add types to component ProgramItemList

--- src/components/programItemList/ProgramItemList.types.ts
+++ src/components/programItemList/ProgramItemList.types.ts
@@ -1,9 +1,17 @@
+interface DropdownItems {
+  id: number;
+  leftIcon: string;
+  onClick(): void;
+  show: boolean;
+  text: string;
+}
+
 interface Props {
   id: number;
   image: string;
   title: string;
   summary: string;
-  dropdownItems?: any[];
+  dropdownItems?: DropdownItems[];
   isLive?: boolean;
   isDraft?: boolean;
   handleClickItem(id: number): void;
