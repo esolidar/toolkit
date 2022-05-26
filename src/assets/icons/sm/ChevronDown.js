c2429d886fe9a6315790e99d35f0ad75b8c799ef
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2022-05-26 15:45:51 +0100

    ENG-2018 #comment update svgs icons (#728)

--- src/assets/icons/sm/ChevronDown.js
+++ src/assets/icons/sm/ChevronDown.js
@@ -3,18 +3,20 @@ import * as React from 'react';
 const SvgChevronDown = props => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    viewBox={props.viewBox}
+    style={{
+      enableBackground: 'new 0 0 16 16',
+    }}
+    xmlSpace="preserve"
     width={props.width}
     height={props.height}
-    fill="none"
-    viewBox={props.viewBox}
     {...props}
   >
     <path
-      stroke="#6C7679"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={1.5}
-      d="m2 6 5.528 5.804a.668.668 0 0 0 .944 0L14 6"
+      d="M8 12.7c-.4 0-.7-.1-1-.4L1.5 6.5c-.3-.3-.3-.8 0-1.1.3-.3.8-.3 1.1 0L8 11.2l5.5-5.7c.3-.3.8-.3 1.1 0 .3.3.3.8 0 1.1L9 12.3c-.3.3-.6.4-1 .4z"
+      style={{
+        fill: '#6c7679',
+      }}
     />
   </svg>
 );
