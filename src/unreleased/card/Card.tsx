d05495aed0e26a3a9e2c2f92c21d31630fd5c021
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2022-06-15 09:50:03 +0100

    ENG-2035 #comment make changes on TicketsComment design (#738)
    * ENG-2035 #comment make changes on TicketsComment design
    
    * ENG-2035 #comment package.json
    
    * ENG-2035 #comment package.json
    
    * TicketsComments
    
    * TicketsComments
    
    * TicketsComments

--- src/unreleased/card/Card.tsx
+++ src/unreleased/card/Card.tsx
@@ -35,7 +35,7 @@ const Card: FC<Props> = ({
         <div
           className="bg-image"
           style={{
-            backgroundImage: `url('${image || urlNoImage}`,
+            backgroundImage: `url('${image || urlNoImage}')`,
             backgroundSize: image ? 'cover' : 'auto',
           }}
         />
