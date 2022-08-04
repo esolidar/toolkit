65b34cd4dd494741bf151b80e5d0e3c8763da4dd
Patrícia Silva <58434703+patricia-mps@users.noreply.github.com>
2022-08-04 11:16:34 +0100

    ENG-2283 #comment fix z-index react-color and error prop (#782)
    * ENG-2283 #comment add new component SelectColor
    
    * ENG-2283 #comment add new component SelectColor
    
    * ENG-2283 #comment add new component form/SelectColor
    
    * ENG-2283 #comment fix z-index react-color and error prop
    
    * ENG-2283 #comment add direct placeholder prop
    
    * ENG-2283 #comment add sm width to react-color

--- src/elements/selectColor/SelectColor.types.ts
+++ src/elements/selectColor/SelectColor.types.ts
@@ -8,6 +8,8 @@ interface Props {
   name: string;
   size?: 'sm' | 'md' | 'lg';
   className?: string;
+  error?: string;
+  placeholder?: string;
   colors?: string[];
   trianglePosition?: 'hide' | 'top-left' | 'top-right' | 'top-left';
   onChange(color: string): any;
