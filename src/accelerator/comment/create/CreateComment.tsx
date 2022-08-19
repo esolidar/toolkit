e0908d09032bdc60c843d1a5e9b80badc709c1a0
Nuno Lemos <39536752+itsnunolemos@users.noreply.github.com>
2022-08-19 09:28:12 +0100

    Fix max length in comments (#795)

--- src/accelerator/comment/create/CreateComment.tsx
+++ src/accelerator/comment/create/CreateComment.tsx
@@ -450,7 +450,7 @@ const Comment: FC<CommentProps> = ({
             value={text.replace(/<\/?[^>]+(>|$)/g, '')}
             dataTestId={`text-${parentId || 0}`}
             autofocus={reference?.current || editMode}
-            maxLength={65535}
+            maxLength={64000}
             isMaxLengthValueVisible={false}
           />
         </div>
