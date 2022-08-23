55c6a54578a797e91a4ba9db0f402c6f0bac4a09
Joel Calheiros <joel@esolidar.com>
2022-08-23 13:01:29 +0100

    Feature eng 2577 (#798)
    * ENG-2577 ENG-2579 #comment updated CheckboxCard component
    
    * ENG-2577 ENG-2579 #comment updated CheckboxCard component

--- src/elements/checkboxCard/CheckboxCard.stories.tsx
+++ src/elements/checkboxCard/CheckboxCard.stories.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react';
 import CheckboxCard from './CheckboxCard';
 import Props from './CheckboxCard.types';
@@ -27,17 +27,27 @@ const Template: Story<Props> = (args: Props) => {
 };
 
 export const Small = Template.bind({});
+export const CardGridItem = Template.bind({});
 export const Medium = Template.bind({});
 export const Large = Template.bind({});
+export const RowItem = Template.bind({});
 export const LargeWithDropdown = Template.bind({});
 
 Small.args = {
   id: 'checkboxCard-sm',
-  defaultImg: `${cdnUploadsUrl}/interests/test-interests.svg`,
-  disabledImg: `${cdnUploadsUrl}/interests/test-interests-disabled.svg`,
-  checkedImg: `${cdnUploadsUrl}/interests/test-interests-checked.svg`,
+  defaultImg: `${cdnUploadsUrl}/interests/v2/default/biodiversity-ecosystems.svg`,
+  disabledImg: `${cdnUploadsUrl}/interests/v2/disabled/biodiversity-ecosystems.svg`,
+  checkedImg: `${cdnUploadsUrl}/interests/v2/active/biodiversity-ecosystems.svg`,
   size: 'sm',
   title: 'Reading & Writing',
+};
+
+CardGridItem.args = {
+  id: 'checkboxCard-CardGridItem',
+  icon: 'Nonprofit',
+  title: 'Nonprofits',
+  fullWith: true,
+  size: 'sm',
 };
 
 Medium.args = {
@@ -55,6 +65,16 @@ Large.args = {
     'End hunger, achieve food security and improved nutrition and promote sustainable agriculture',
   title: 'Zero hunger',
   className: 'large',
+};
+RowItem.args = {
+  id: 'checkboxCard-RowItem',
+  defaultImg:
+    'https://esolidar-proto-uploads.s3.eu-west-1.amazonaws.com/institutions/5db97fcf-b9b4-4ae5-8c49-23e4ac1f17c3.png',
+  size: 'lg',
+  title: 'Fundo Brasileiro para a Biodiversidade',
+  className: 'large',
+  fullWith: true,
+  roundedIcon: true,
 };
 
 LargeWithDropdown.args = {
--- src/elements/checkboxCard/CheckboxCard.types.ts
+++ src/elements/checkboxCard/CheckboxCard.types.ts
@@ -3,7 +3,7 @@ import { Item } from '../dropdown/Dropdown.types';
 interface Props {
   disabled?: boolean;
   id?: string;
-  defaultImg: string;
+  defaultImg?: string;
   disabledHover?: boolean;
   disabledImg?: string;
   checkedImg?: string;
@@ -16,6 +16,9 @@ interface Props {
   className?: string;
   style?: React.CSSProperties;
   dropdownItems?: Item[];
+  icon?: string;
+  fullWith?: boolean;
+  roundedIcon?: boolean;
 }
 
 export default Props;
