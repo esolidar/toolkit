c579044c75931ab09c275219b14eb5a8b9e512c2
Joel Calheiros <joel@esolidar.com>
2022-08-23 16:59:15 +0100

    ENG-2605 #comment Added custom header

--- src/elements/customModal/__tests__/CustomModal.test.js
+++ src/elements/customModal/__tests__/CustomModal.test.js
@@ -4,10 +4,12 @@ import { render, fireEvent } from '../../../../__customQueries__/test-utils';
 import Meta, {
   Default as DefaultStory,
   WithScroll as WithScrollStory,
+  CustomHeader as CustomHeaderStory,
 } from '../CustomModal.stories';
 
 const Default = composeStory(DefaultStory, Meta);
 const WithScroll = composeStory(WithScrollStory, Meta);
+const CustomHeader = composeStory(CustomHeaderStory, Meta);
 
 describe('CustomModal component', () => {
   it('renders "CustomModal" correctly', () => {
@@ -49,5 +51,13 @@ describe('CustomModal component', () => {
     expect(getByClass('modal-header')).toHaveStyle({
       borderBottom: '1px solid rgb(222, 226, 230',
     });
+  });
+
+  it('renders modal with custom header', async () => {
+    const { getByClass, getByTestId } = render(<CustomHeader />);
+
+    expect(getByTestId('btn-back')).toBeInTheDocument();
+    expect(getByClass('custom-modal__bar')).toBeInTheDocument();
+    expect(getByClass('custom-modal__bar')).toBeInTheDocument();
   });
 });
